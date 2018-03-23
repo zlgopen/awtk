8d7aaf09a55593bb9700d1d3fc4517c9d06ce1c7
lixianjing <xianjimli@gmail.com>
2018-03-23 19:15:24 +0800

    add matrix

---
+++ src/base/matrix.c
@@ -0,0 +1,91 @@
+﻿/**
+ * File:   matrix.c
+ * Author: Li XianJing <xianjimli@hotmail.com>
+ * Brief:  matrix
+ *
+ * Copyright (c) 2018 - 2018  Li XianJing <xianjimli@hotmail.com>
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; withm even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-03-23 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include <math.h>
+#include "base/matrix.h"
+
+matrix_t* matrix_init(matrix_t* m) {
+  return_value_if_fail(m != NULL, NULL);
+
+  return matrix_identity(m);
+}
+
+matrix_t* matrix_identity(matrix_t* m) {
+  return_value_if_fail(m != NULL, NULL);
+
+  m->a0 = 1;
+  m->a1 = 0;
+  m->a2 = 0;
+  m->a3 = 1;
+  m->a4 = 0;
+  m->a5 = 0;
+
+  return m;
+}
+
+matrix_t* matrix_translate(matrix_t* m, xy_t x, xy_t y) {
+  float a0 = m->a0, a1 = m->a1, a2 = m->a2, a3 = m->a3, a4 = m->a4, a5 = m->a5;
+
+  m->a0 = a0;
+  m->a1 = a1;
+  m->a2 = a2;
+  m->a3 = a3;
+  m->a4 = a0 * x + a2 * y + a4;
+  m->a5 = a1 * x + a3 * y + a5;
+
+  return m;
+}
+
+matrix_t* matrix_scale(matrix_t* m, float sx, float sy) {
+  float a0 = m->a0, a1 = m->a1, a2 = m->a2, a3 = m->a3, a4 = m->a4, a5 = m->a5;
+  m->a0 = a0 * sx;
+  m->a1 = a1 * sx;
+  m->a2 = a2 * sy;
+  m->a3 = a3 * sy;
+  m->a4 = a4;
+  m->a5 = a5;
+
+  return m;
+}
+
+matrix_t* matrix_rotate(matrix_t* m, float rad) {
+  float a0 = m->a0, a1 = m->a1, a2 = m->a2, a3 = m->a3, a4 = m->a4, a5 = m->a5, s = sin(rad),
+        c = cos(rad);
+
+  m->a0 = a0 * c + a2 * s;
+  m->a1 = a1 * c + a3 * s;
+  m->a2 = a0 * -s + a2 * c;
+  m->a3 = a1 * -s + a3 * c;
+  m->a4 = a4;
+  m->a5 = a5;
+
+  return m;
+}
+
+matrix_t* matrix_transform_debug(matrix_t* m, xy_t x, xy_t y, xy_t* ox, xy_t* oy) {
+  float x1 = m->a0 * x + m->a2 * y + m->a4;
+  float y1 = m->a1 * x + m->a3 * y + m->a5;
+
+  *ox = x1 > 0 ? x1 + 0.5 : x1 - 0.5;
+  *oy = y1 > 0 ? y1 + 0.5 : y1 - 0.5;
+
+  return m;
+}
---
+++ src/base/matrix.h
@@ -0,0 +1,55 @@
+﻿/**
+ * File:   matrix.h
+ * Author: Li XianJing <xianjimli@hotmail.com>
+ * Brief:  matrix struct and utils functions.
+ *
+ * Copyright (c) 2018 - 2018  Li XianJing <xianjimli@hotmail.com>
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-03-23 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#ifndef LFTK_MATRIX_H
+#define LFTK_MATRIX_H
+
+#include "base/types_def.h"
+
+BEGIN_C_DECLS
+
+typedef struct _matrix_t {
+  float a0;
+  float a1;
+  float a2;
+  float a3;
+  float a4;
+  float a5;
+} matrix_t;
+
+matrix_t* matrix_init(matrix_t* m);
+
+matrix_t* matrix_identity(matrix_t* m);
+matrix_t* matrix_translate(matrix_t* m, xy_t x, xy_t y);
+matrix_t* matrix_scale(matrix_t* m, float sx, float sy);
+matrix_t* matrix_rotate(matrix_t* m, float rad);
+matrix_t* matrix_transform_debug(matrix_t* m, xy_t x, xy_t y, xy_t* out_x, xy_t* out_y);
+
+#define matrix_transform(_m, _x, _y, _ox, _oy) \
+  tempx = _m.a0 * _x + _m.a2 * _y + _m.a4;\
+  tempy = _m.a1 * _x + _m.a3 * _y + _m.a5;\
+  _ox = tempx > 0 ? tempx + 0.5 : tempx - 0.5;\
+  _oy = tempy > 0 ? tempy + 0.5 : tempy - 0.5;
+
+END_C_DECLS
+
+#endif /*LFTK_MATRIX_H*/
+
