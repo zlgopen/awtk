cb032566f0eb3fab8bee91f4aa1b996083c432dd
xianjimli <xianjimli@gmail.com>
2018-04-05 17:59:53 +0800

    add view

---
+++ src/base/view.c
@@ -0,0 +1,40 @@
+﻿/**
+ * File:   view.h
+ * Author: Li XianJing <xianjimli@hotmail.com>
+ * Brief:  view
+ *
+ * Copyright (c) 2018 - 2018  Li XianJing <xianjimli@hotmail.com>
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-02-11 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "base/mem.h"
+#include "base/view.h"
+
+static const widget_vtable_t s_view_vtable = {};
+
+widget_t* view_create(widget_t* parent, xy_t x, xy_t y, wh_t w, wh_t h) {
+  widget_t* widget = NULL;
+  view_t* view = MEM_ZALLOC(view_t);
+  return_value_if_fail(view != NULL, NULL);
+
+  widget = WIDGETP(view);
+  widget_init(widget, parent, WIDGET_VIEW);
+  widget_move_resize(widget, x, y, w, h);
+  widget->vt = &s_view_vtable;
+
+  widget_set_state(widget, WIDGET_STATE_NORMAL);
+
+  return widget;
+}
