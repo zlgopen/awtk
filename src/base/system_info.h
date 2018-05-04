2b31c1efd61eff58218ad91d29c48ab2ce014fb5
xianjimli <xianjimli@gmail.com>
2018-05-04 17:32:02 +0800

    update docs

--- src/base/system_info.h
+++ src/base/system_info.h
@@ -26,15 +26,49 @@
 
 BEGIN_C_DECLS
 
+/**
+ * @class system_info_t
+ * 当前系统的信息。
+ */
 typedef struct _system_info_t {
+  /**
+   * @property {uint32_t} lcd_w
+   * @readonly
+   * 显示屏的宽度。
+   */
   uint32_t lcd_w;
+  
+  /**
+   * @property {uint32_t} lcd_h
+   * @readonly
+   * 显示屏的高度。
+   */
   uint32_t lcd_h;
+
+  /**
+   * @property {lcd_type_t} lcd_type
+   * @readonly
+   * 显示屏的类型。
+   */
   lcd_type_t lcd_type;
+
+  /**
+   * @property {float_t} device_pixel_ratio
+   * @readonly
+   * 显示屏的密度。
+   */
   float_t device_pixel_ratio;
 } system_info_t;
 
+/**
+ * @method system_info
+ * 获取system_info对象。
+ * @constructor
+ * @return {system_info_t*} 返回system_info对象。
+ */
 system_info_t* system_info(void);
 
 END_C_DECLS
 
 #endif /*TK_SYSTEM_INFO_H*/
+
