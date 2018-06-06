fc78ad06d62fb46d07335a0a94f283c7091d796b
xianjimli <xianjimli@gmail.com>
2018-06-06 15:57:03 +0800

    add widget_animator_scroll

---
+++ src/widget_animators/widget_animator_scroll.c
@@ -0,0 +1,72 @@
+﻿/**
+ * File:   widget_animator_scroll.h
+ * Author: AWTK Develop Team
+ * Brief:  animate widget by change its position.
+ *
+ * Copyright (c) 2018 - 2018  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-05-15 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "base/mem.h"
+#include "base/prop_names.h"
+#include "widget_animators/widget_animator_scroll.h"
+
+static ret_t widget_animator_scroll_update(widget_animator_t* animator, float_t percent) {
+  int xoffset = 0;
+  int yoffset = 0;
+  value_t v;
+  widget_animator_scroll_t* scroll = (widget_animator_scroll_t*)animator;
+  return_value_if_fail(scroll != NULL, RET_BAD_PARAMS);
+
+  if(scroll->x_to != scroll->x_from) {
+    xoffset = scroll->x_from + (scroll->x_to - scroll->x_from) * percent;
+    value_set_int(&v, xoffset);
+    widget_set_prop(animator->widget, WIDGET_PROP_XOFFSET, &v);
+  }
+
+  if(scroll->y_to != scroll->y_from) {
+    yoffset = scroll->y_from + (scroll->y_to - scroll->y_from) * percent;
+    value_set_int(&v, yoffset);
+    widget_set_prop(animator->widget, WIDGET_PROP_YOFFSET, &v);
+  }
+
+  return RET_OK;
+}
+
+widget_animator_t* widget_animator_scroll_create(widget_t* widget, uint32_t duration, uint32_t delay,
+                                               easing_type_t easing) {
+  widget_animator_t* animator = NULL;
+  return_value_if_fail(widget != NULL && duration > 0, NULL);
+
+  animator = (widget_animator_t*)TKMEM_ZALLOC(widget_animator_scroll_t);
+  return_value_if_fail(
+      widget_animator_init(animator, widget, duration, delay, easing_get(easing)) == RET_OK, NULL);
+  animator->update = widget_animator_scroll_update;
+
+  return animator;
+}
+
+ret_t widget_animator_scroll_set_params(widget_animator_t* animator, xy_t x_from, xy_t y_from,
+                                      xy_t x_to, xy_t y_to) {
+  widget_animator_scroll_t* scroll = (widget_animator_scroll_t*)animator;
+  return_value_if_fail(scroll != NULL, RET_BAD_PARAMS);
+
+  scroll->x_to = x_to;
+  scroll->y_to = y_to;
+  scroll->x_from = x_from;
+  scroll->y_from = y_from;
+
+  return RET_OK;
+}
---
+++ src/widget_animators/widget_animator_scroll.h
@@ -0,0 +1,71 @@
+﻿/**
+ * File:   widget_animator_scroll.h
+ * Author: AWTK Develop Team
+ * Brief:  animate widget by change its position.
+ *
+ * Copyright (c) 2018 - 2018  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-05-15 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#ifndef TK_WIDGET_ANIMATOR_SCROLL_H
+#define TK_WIDGET_ANIMATOR_SCROLL_H
+
+#include "base/widget_animator.h"
+
+BEGIN_C_DECLS
+
+/**
+ * @class widget_animator_scroll_t
+ * 移动控件位置的动画。
+ */
+typedef struct _widget_animator_scroll_t {
+  widget_animator_t base;
+
+  xy_t x_to;
+  xy_t y_to;
+  xy_t x_from;
+  xy_t y_from;
+} widget_animator_scroll_t;
+
+/**
+ * @method widget_animator_scroll_create
+ * 创建动画对象。
+ * @param {widget_t*} widget 控件对象。
+ * @param {uint32_t} duration 动画持续时间。
+ * @param {uint32_t} delay 动画执行时间。
+ * @param {easing_type_t} easing 插值函数类型。
+ *
+ * @return {widget_animator_t*} 成功返回动画对象，失败返回NULL。
+ */
+widget_animator_t* widget_animator_scroll_create(widget_t* widget, uint32_t duration, uint32_t delay,
+                                               easing_type_t easing);
+
+/**
+ * @method widget_animator_scroll_set_params
+ * 设置动画对象的参数。
+ * @param {widget_animator_t*} animator 动画对象本身。
+ * @param {xy_t} x_from x起点值。
+ * @param {xy_t} y_from y起点值。
+ * @param {xy_t} x_to x终点值。
+ * @param {xy_t} y_to y终点值。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t widget_animator_scroll_set_params(widget_animator_t* animator, xy_t x_from, xy_t y_from,
+                                      xy_t x_to, xy_t y_to);
+
+END_C_DECLS
+
+#endif /*TK_WIDGET_ANIMATOR_SCROLL_H*/
