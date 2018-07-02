9c284dd14407057cf98f0ca878a67d4ab6455232
xianjimli <xianjimli@gmail.com>
2018-07-02 18:23:22 +0800

    fix for stmf103

--- src/base/widget_factory.c
+++ src/base/widget_factory.c
@@ -65,11 +65,11 @@ static const creator_item_t s_builtin_creators[] = {
     {WIDGET_TYPE_PAGES, pages_create},
     {WIDGET_TYPE_SPIN_BOX, spin_box_create},
     {WIDGET_TYPE_DRAGGER, dragger_create},
+#ifndef WITH_LOW_RES
     {WIDGET_TYPE_SCROLL_BAR, scroll_bar_create},
     {WIDGET_TYPE_SCROLL_BAR_DESKTOP, scroll_bar_create_desktop},
     {WIDGET_TYPE_SCROLL_BAR_MOBILE, scroll_bar_create_mobile},
 
-#ifndef WITH_LOW_RES
     {WIDGET_TYPE_SLIDE_VIEW, slide_view_create},
     {WIDGET_TYPE_KEYBOARD, keyboard_create},
     {WIDGET_TYPE_CANDIDATES, candidates_create},
