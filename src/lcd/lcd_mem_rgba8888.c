8d715a4d266e5a0549b7dd4b00c1032698c89287
xianjimli <xianjimli@gmail.com>
2018-07-07 17:59:50 +0800

    fix rgba8888

--- src/lcd/lcd_mem_rgba8888.c
+++ src/lcd/lcd_mem_rgba8888.c
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * File:   lcd_mem.c
  * Author: AWTK Develop Team
  * Brief:  mem implemented lcd interface/
@@ -43,5 +43,4 @@ lcd_t* lcd_mem_rgba8888_create_double_fb(wh_t w, wh_t h, uint8_t* online_fb, uin
 
 lcd_t* lcd_mem_rgba8888_create_three_fb(wh_t w, wh_t h, uint8_t* online_fb, uint8_t* offline_fb, uint8_t* next_fb) {
   return lcd_mem_create_three_fb(w, h, online_fb, offline_fb, next_fb);
-}
 }
