6b1aefa78bf0cb83647f83d1254b9c6f08ff40a1
xianjimli <xianjimli@gmail.com>
2018-08-04 17:11:32 +0800

    fix open_window

--- demos/demo_ui_app.c
+++ demos/demo_ui_app.c
@@ -43,7 +43,7 @@ static void open_window(const char* name, widget_t* to_close) {
 
   install_click_hander(win);
 
-  if (widget_get_type(win) == WIDGET_TYPE_DIALOG) {
+  if (tk_str_eq(widget_get_type(win), WIDGET_TYPE_DIALOG)) {
     dialog_modal(win);
   }
 }
