e09444412b7c3b9e82becdc326c4b65a1b032417
xianjimli <xianjimli@gmail.com>
2018-08-07 17:58:05 +0800

    improve api docs

--- src/base/widget_consts.h
+++ src/base/widget_consts.h
@@ -27,123 +27,711 @@
 BEGIN_C_DECLS
 
 /*widget props*/
+
+/**
+ * @enum widget_prop_t 
+ * @annotation ["scriptable", "string"]
+ * @prefix WIDGET_PROP_
+ * 控件的属性。
+ */
+
+/**
+ * @const WIDGET_PROP_X
+ * X坐标。
+ */
 #define WIDGET_PROP_X "x"
+
+/**
+ * @const WIDGET_PROP_Y
+ * Y坐标。
+ */
 #define WIDGET_PROP_Y "y"
+
+/**
+ * @const WIDGET_PROP_W
+ * 宽度。
+ */
 #define WIDGET_PROP_W "w"
+
+/**
+ * @const WIDGET_PROP_H
+ * 高度。
+ */
 #define WIDGET_PROP_H "h"
+
+/**
+ * @const WIDGET_PROP_MIN_W
+ * 最小宽度。
+ */
 #define WIDGET_PROP_MIN_W "min_w"
+
+/**
+ * @const WIDGET_PROP_MAX_W
+ * 最大宽度。
+ */
 #define WIDGET_PROP_MAX_W "max_w"
+
+/**
+ * @const WIDGET_PROP_LAYOUT_W
+ * layout宽度。
+ */
 #define WIDGET_PROP_LAYOUT_W "layout_w"
+
+/**
+ * @const WIDGET_PROP_LAYOUT_H
+ * layout高度。
+ */
 #define WIDGET_PROP_LAYOUT_H "layout_h"
+
+/**
+ * @const WIDGET_PROP_VIRTUAL_W
+ * 虚拟宽度。
+ */
 #define WIDGET_PROP_VIRTUAL_W "virtual_w"
+
+/**
+ * @const WIDGET_PROP_VIRTUAL_H
+ * 虚拟高度。
+ */
 #define WIDGET_PROP_VIRTUAL_H "virtual_h"
+
+/**
+ * @const WIDGET_PROP_NAME
+ * 名称。
+ */
 #define WIDGET_PROP_NAME "name"
+
+/**
+ * @const WIDGET_PROP_VALUE
+ * 值。
+ */
 #define WIDGET_PROP_VALUE "value"
+
+/**
+ * @const WIDGET_PROP_TEXT
+ * 文本。
+ */
 #define WIDGET_PROP_TEXT "text"
+
+/**
+ * @const WIDGET_PROP_TR_TEXT
+ * 待翻译文本。
+ */
 #define WIDGET_PROP_TR_TEXT "tr_text"
+
+/**
+ * @const WIDGET_PROP_STYLE
+ * style。
+ */
 #define WIDGET_PROP_STYLE "style"
+
+/**
+ * @const WIDGET_PROP_ENABLE
+ * 是否启用。
+ */
 #define WIDGET_PROP_ENABLE "enable"
+
+/**
+ * @const WIDGET_PROP_MARGIN
+ * 边距。
+ */
 #define WIDGET_PROP_MARGIN "margin"
+
+/**
+ * @const WIDGET_PROP_SPACING
+ * 间距。
+ */
 #define WIDGET_PROP_SPACING "spacing"
+
+/**
+ * @const WIDGET_PROP_LEFT_MARGIN
+ * 左边距。
+ */
 #define WIDGET_PROP_LEFT_MARGIN "left_margin"
+
+/**
+ * @const WIDGET_PROP_RIGHT_MARGIN
+ * 右边距。
+ */
 #define WIDGET_PROP_RIGHT_MARGIN "right_margin"
+
+/**
+ * @const WIDGET_PROP_TOP_MARGIN
+ * 顶边距。
+ */
 #define WIDGET_PROP_TOP_MARGIN "top_margin"
+
+/**
+ * @const WIDGET_PROP_BOTTOM_MARGIN
+ * 底边距。
+ */
 #define WIDGET_PROP_BOTTOM_MARGIN "bottom_margin"
+
+/**
+ * @const WIDGET_PROP_STEP
+ * 步长。
+ */
 #define WIDGET_PROP_STEP "step"
+
+/**
+ * @const WIDGET_PROP_VISIBLE
+ * 是否可见。
+ */
 #define WIDGET_PROP_VISIBLE "visible"
+
+/**
+ * @const WIDGET_PROP_VISIBLE
+ * 窗口动画。
+ */
 #define WIDGET_PROP_ANIM_HINT "anim_hint"
+
+/**
+ * @const WIDGET_PROP_OPEN_ANIM_HINT
+ * 打开窗口动画。
+ */
 #define WIDGET_PROP_OPEN_ANIM_HINT "open_anim_hint"
-#define WIDGET_PROP_CLOSE_ANIM_HINT "opne_anim_hint"
+
+/**
+ * @const WIDGET_PROP_CLOSE_ANIM_HINT
+ * 关闭窗口动画。
+ */
+#define WIDGET_PROP_CLOSE_ANIM_HINT "close_anim_hint"
+
+/**
+ * @const WIDGET_PROP_MIN
+ * 
+ */
 #define WIDGET_PROP_MIN "min"
+
+/**
+ * @const WIDGET_PROP_MAX
+ * 
+ */
 #define WIDGET_PROP_MAX "max"
+
+/**
+ * @const WIDGET_PROP_TIPS
+ * 
+ */
 #define WIDGET_PROP_TIPS "tips"
+
+/**
+ * @const WIDGET_PROP_INPUT_TYPE
+ * 
+ */
 #define WIDGET_PROP_INPUT_TYPE "input_type"
+
+/**
+ * @const WIDGET_PROP_READONLY
+ * 
+ */
 #define WIDGET_PROP_READONLY "readonly"
+
+/**
+ * @const WIDGET_PROP_PASSWORD_VISIBLE
+ * 
+ */
 #define WIDGET_PROP_PASSWORD_VISIBLE "password_visible"
+
+/**
+ * @const WIDGET_PROP_ACTIVE
+ * 
+ */
 #define WIDGET_PROP_ACTIVE "active"
+
+/**
+ * @const WIDGET_PROP_VERTICAL
+ * 
+ */
 #define WIDGET_PROP_VERTICAL "vertical"
+
+/**
+ * @const WIDGET_PROP_SHOW_TEXT
+ * 
+ */
 #define WIDGET_PROP_SHOW_TEXT "show_text"
+
+/**
+ * @const WIDGET_PROP_XOFFSET
+ * 
+ */
 #define WIDGET_PROP_XOFFSET "xoffset"
+
+/**
+ * @const WIDGET_PROP_YOFFSET
+ * 
+ */
 #define WIDGET_PROP_YOFFSET "yoffset"
+
+/**
+ * @const WIDGET_PROP_AUTO_PLAY
+ * 
+ */
 #define WIDGET_PROP_AUTO_PLAY "auto_play"
+
+/**
+ * @const WIDGET_PROP_AUTO_FIX
+ * 
+ */
 #define WIDGET_PROP_AUTO_FIX "auto_fix"
+
+/**
+ * @const WIDGET_PROP_X_MIN
+ * 
+ */
 #define WIDGET_PROP_X_MIN "x_min"
+
+/**
+ * @const WIDGET_PROP_X_MAX
+ * 
+ */
 #define WIDGET_PROP_X_MAX "x_max"
+
+/**
+ * @const WIDGET_PROP_Y_MIN
+ * 
+ */
 #define WIDGET_PROP_Y_MIN "y_min"
+
+/**
+ * @const WIDGET_PROP_Y_MAX
+ * 
+ */
 #define WIDGET_PROP_Y_MAX "y_max"
+
+/**
+ * @const WIDGET_PROP_MAX
+ * 
+ */
 #define WIDGET_PROP_MAX "max"
+
+/**
+ * @const WIDGET_PROP_ROW
+ * 
+ */
 #define WIDGET_PROP_ROW "row"
+
+/**
+ * @const WIDGET_PROP_THEME
+ * 
+ */
 #define WIDGET_PROP_THEME "theme"
+
+/**
+ * @const WIDGET_PROP_SCRIPT
+ * 
+ */
 #define WIDGET_PROP_SCRIPT "script"
+
+/**
+ * @const WIDGET_PROP_ITEM_WIDTH
+ * 
+ */
 #define WIDGET_PROP_ITEM_WIDTH "item_width"
+
+/**
+ * @const WIDGET_PROP_ITEM_HEIGHT
+ * 
+ */
 #define WIDGET_PROP_ITEM_HEIGHT "item_height"
+
+/**
+ * @const WIDGET_PROP_DEFAULT_ITEM_HEIGHT
+ * 
+ */
 #define WIDGET_PROP_DEFAULT_ITEM_HEIGHT "default_item_height"
+
+/**
+ * @const WIDGET_PROP_XSLIDABLE
+ * 
+ */
 #define WIDGET_PROP_XSLIDABLE "xslidable"
+
+/**
+ * @const WIDGET_PROP_YSLIDABLE
+ * 
+ */
 #define WIDGET_PROP_YSLIDABLE "yslidable"
+
+/**
+ * @const WIDGET_PROP_REPEAT
+ * 
+ */
 #define WIDGET_PROP_REPEAT "repeat"
+
+/**
+ * @const WIDGET_PROP_ANIMATABLE
+ * 
+ */
 #define WIDGET_PROP_ANIMATABLE "animatable"
+
+/**
+ * @const WIDGET_PROP_AUTO_HIDE_SCROLL_BAR
+ * 
+ */
 #define WIDGET_PROP_AUTO_HIDE_SCROLL_BAR "auto_hide_scroll_bar"
+
+/**
+ * @const WIDGET_PROP_IMAGE
+ * 
+ */
 #define WIDGET_PROP_IMAGE "image"
+
+/**
+ * @const WIDGET_PROP_DRAW_TYPE
+ * 
+ */
 #define WIDGET_PROP_DRAW_TYPE "draw_type"
+
+/**
+ * @const WIDGET_PROP_SELECTABLE
+ * 
+ */
 #define WIDGET_PROP_SELECTABLE "selectable"
+
+/**
+ * @const WIDGET_PROP_CLICKABLE
+ * 
+ */
 #define WIDGET_PROP_CLICKABLE "clickable"
+
+/**
+ * @const WIDGET_PROP_SCALE_X
+ * 
+ */
 #define WIDGET_PROP_SCALE_X "scale_x"
+
+/**
+ * @const WIDGET_PROP_SCALE_Y
+ * 
+ */
 #define WIDGET_PROP_SCALE_Y "scale_y"
+
+/**
+ * @const WIDGET_PROP_ANCHOR_X
+ * x锚点。 
+ */
 #define WIDGET_PROP_ANCHOR_X "anchor_x"
+
+/**
+ * @const WIDGET_PROP_ANCHOR_Y
+ * y锚点。 
+ */
 #define WIDGET_PROP_ANCHOR_Y "anchor_y"
+
+/**
+ * @const WIDGET_PROP_ROTATION
+ * 选中角度(幅度) 
+ */
 #define WIDGET_PROP_ROTATION "rotation"
+
+/**
+ * @const WIDGET_PROP_SUB_THEME
+ * sub theme。 
+ */
 #define WIDGET_PROP_SUB_THEME "sub_theme"
+
+/**
+ * @const WIDGET_PROP_COMPACT
+ * 紧凑模式。 
+ */
 #define WIDGET_PROP_COMPACT "compact"
+
+/**
+ * @const WIDGET_PROP_ICON
+ * 图标名称。 
+ */
 #define WIDGET_PROP_ICON "icon"
+
+/**
+ * @const WIDGET_PROP_OPTIONS
+ * 选项集合。 
+ */
 #define WIDGET_PROP_OPTIONS "options"
+
+/**
+ * @const WIDGET_PROP_SELECTED
+ * 是否被选中。 
+ */
 #define WIDGET_PROP_SELECTED "selected"
+
+/**
+ * @const WIDGET_PROP_ACTIVE_ICON
+ * active状态下的图标。 
+ */
 #define WIDGET_PROP_ACTIVE_ICON "active_icon"
+
+/**
+ * @const WIDGET_PROP_OPEN_WINDOW
+ * 要打开窗口的名称。 
+ */
 #define WIDGET_PROP_OPEN_WINDOW "open_window"
+
+/**
+ * @const WIDGET_PROP_SELECTED_INDEX
+ * 被选中项的索引。 
+ */
 #define WIDGET_PROP_SELECTED_INDEX "selected_index"
+
+/**
+ * @const WIDGET_PROP_CLOSE_WHEN_CLICK
+ * 点击窗口时关闭窗口。 
+ */
 #define WIDGET_PROP_CLOSE_WHEN_CLICK "close_when_click"
+
+/**
+ * @const WIDGET_PROP_CLOSE_WHEN_CLICK_OUTSIDE
+ * 点击窗口外部时关闭窗口。 
+ */
 #define WIDGET_PROP_CLOSE_WHEN_CLICK_OUTSIDE "close_when_click_outside"
+
+/**
+ * @const WIDGET_PROP_LINE_GAP
+ * 行间距。 
+ */
 #define WIDGET_PROP_LINE_GAP "line_gap"
 
-/*widget type name*/
+/**
+ * @enum widget_type_t 
+ * @annotation ["scriptable", "string"]
+ * @prefix WIDGET_TYPE_
+ * 控件的类型。
+ */
+
+/**
+ * @const WIDGET_TYPE_NONE
+ * 
+ */
 #define WIDGET_TYPE_NONE "widget"
+
+/**
+ * @const WIDGET_TYPE_WINDOW_MANAGER
+ * 
+ */
 #define WIDGET_TYPE_WINDOW_MANAGER "window_manager"
+
+/**
+ * @const WIDGET_TYPE_NORMAL_WINDOW
+ * 
+ */
 #define WIDGET_TYPE_NORMAL_WINDOW "window"
+
+/**
+ * @const WIDGET_TYPE_TOOL_BAR
+ * 
+ */
 #define WIDGET_TYPE_TOOL_BAR "tool_bar"
+
+/**
+ * @const WIDGET_TYPE_DIALOG
+ * 
+ */
 #define WIDGET_TYPE_DIALOG "dialog"
+
+/**
+ * @const WIDGET_TYPE_POPUP
+ * 
+ */
 #define WIDGET_TYPE_POPUP "popup"
+
+/**
+ * @const WIDGET_TYPE_SPRITE
+ * 
+ */
 #define WIDGET_TYPE_SPRITE "sprite"
+
+/**
+ * @const WIDGET_TYPE_KEYBOARD
+ * 
+ */
 #define WIDGET_TYPE_KEYBOARD "keyboard"
+
+/**
+ * @const WIDGET_TYPE_DND
+ * 
+ */
 #define WIDGET_TYPE_DND "dnd"
+
+/**
+ * @const WIDGET_TYPE_LABEL
+ * 
+ */
 #define WIDGET_TYPE_LABEL "label"
+
+/**
+ * @const WIDGET_TYPE_BUTTON
+ * 
+ */
 #define WIDGET_TYPE_BUTTON "button"
+
+/**
+ * @const WIDGET_TYPE_IMAGE
+ * 
+ */
 #define WIDGET_TYPE_IMAGE "image"
+
+/**
+ * @const WIDGET_TYPE_EDIT
+ * 
+ */
 #define WIDGET_TYPE_EDIT "edit"
+
+/**
+ * @const WIDGET_TYPE_PROGRESS_BAR
+ * 
+ */
 #define WIDGET_TYPE_PROGRESS_BAR "progress_bar"
+
+/**
+ * @const WIDGET_TYPE_GROUP_BOX
+ * 
+ */
 #define WIDGET_TYPE_GROUP_BOX "group_box"
+
+/**
+ * @const WIDGET_TYPE_CHECK_BUTTON
+ * 
+ */
 #define WIDGET_TYPE_CHECK_BUTTON "check_button"
+
+/**
+ * @const WIDGET_TYPE_RADIO_BUTTON
+ * 
+ */
 #define WIDGET_TYPE_RADIO_BUTTON "radio_button"
+
+/**
+ * @const WIDGET_TYPE_DIALOG_TITLE
+ * 
+ */
 #define WIDGET_TYPE_DIALOG_TITLE "dialog_title"
+
+/**
+ * @const WIDGET_TYPE_DIALOG_CLIENT
+ * 
+ */
 #define WIDGET_TYPE_DIALOG_CLIENT "dialog_client"
+
+/**
+ * @const WIDGET_TYPE_SLIDER
+ * 
+ */
 #define WIDGET_TYPE_SLIDER "slider"
+
+/**
+ * @const WIDGET_TYPE_VIEW
+ * 
+ */
 #define WIDGET_TYPE_VIEW "view"
+
+/**
+ * @const WIDGET_TYPE_COMBO_BOX
+ * 
+ */
 #define WIDGET_TYPE_COMBO_BOX "combo_box"
+
+/**
+ * @const WIDGET_TYPE_COMBO_BOX_ITEM
+ * 
+ */
 #define WIDGET_TYPE_COMBO_BOX_ITEM "combo_box_item"
+
+/**
+ * @const WIDGET_TYPE_SLIDE_VIEW
+ * 
+ */
 #define WIDGET_TYPE_SLIDE_VIEW "slide_view"
+
+/**
+ * @const WIDGET_TYPE_PAGES
+ * 
+ */
 #define WIDGET_TYPE_PAGES "pages"
+
+/**
+ * @const WIDGET_TYPE_TAB_BUTTON
+ * 
+ */
 #define WIDGET_TYPE_TAB_BUTTON "tab_button"
+
+/**
+ * @const WIDGET_TYPE_TAB_CONTROL
+ * 
+ */
 #define WIDGET_TYPE_TAB_CONTROL "tab_control"
+
+/**
+ * @const WIDGET_TYPE_TAB_BUTTON_GROUP
+ * 
+ */
 #define WIDGET_TYPE_TAB_BUTTON_GROUP "tab_button_group"
+
+/**
+ * @const WIDGET_TYPE_BUTTON_GROUP
+ * 
+ */
 #define WIDGET_TYPE_BUTTON_GROUP "button_group"
+
+/**
+ * @const WIDGET_TYPE_CANDIDATES
+ * 
+ */
 #define WIDGET_TYPE_CANDIDATES "candidates"
+
+/**
+ * @const WIDGET_TYPE_SPIN_BOX
+ * 
+ */
 #define WIDGET_TYPE_SPIN_BOX "spin_box"
+
+/**
+ * @const WIDGET_TYPE_DRAGGER
+ * 
+ */
 #define WIDGET_TYPE_DRAGGER "dragger"
+
+/**
+ * @const WIDGET_TYPE_SCROLL_BAR
+ * 
+ */
 #define WIDGET_TYPE_SCROLL_BAR "scroll_bar"
+
+/**
+ * @const WIDGET_TYPE_SCROLL_BAR_DESKTOP
+ * 
+ */
 #define WIDGET_TYPE_SCROLL_BAR_DESKTOP "scroll_bar_d"
+
+/**
+ * @const WIDGET_TYPE_SCROLL_BAR_MOBILE
+ * 
+ */
 #define WIDGET_TYPE_SCROLL_BAR_MOBILE "scroll_bar_m"
+
+/**
+ * @const WIDGET_TYPE_SCROLL_VIEW
+ * 
+ */
 #define WIDGET_TYPE_SCROLL_VIEW "scroll_view"
+
+/**
+ * @const WIDGET_TYPE_LIST_VIEW
+ * 
+ */
 #define WIDGET_TYPE_LIST_VIEW "list_view"
+
+/**
+ * @const WIDGET_TYPE_LIST_VIEW_H
+ * 
+ */
 #define WIDGET_TYPE_LIST_VIEW_H "list_view_h"
+
+/**
+ * @const WIDGET_TYPE_LIST_ITEM
+ * 
+ */
 #define WIDGET_TYPE_LIST_ITEM "list_item"
+
 
 /**
  * @enum widget_state_t
