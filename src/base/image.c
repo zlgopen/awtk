24238fd503e9ba0c401194f1d8186e6e10ef838b
xianjimli <xianjimli@gmail.com>
2018-10-21 12:21:44 +0800

    improve image

--- src/base/image.c
+++ src/base/image.c
@@ -25,8 +25,6 @@
 #include "base/utils.h"
 #include "base/image_manager.h"
 
-static ret_t image_set_image(widget_t* widget, bitmap_t* image);
-
 static ret_t image_on_event(widget_t* widget, event_t* e) {
   uint16_t type = e->type;
   image_t* image = IMAGE(widget);
@@ -64,12 +62,17 @@ static ret_t image_on_event(widget_t* widget, event_t* e) {
 
 static ret_t image_on_paint_self(widget_t* widget, canvas_t* c) {
   rect_t dst;
-  bitmap_t* bitmap = NULL;
+  bitmap_t bitmap;
   image_t* image = IMAGE(widget);
   vgcanvas_t* vg = lcd_get_vgcanvas(c->lcd);
   return_value_if_fail(widget != NULL && c != NULL, RET_BAD_PARAMS);
 
-  bitmap = &(image->bitmap);
+  if (image->image == NULL) {
+    return RET_OK;
+  }
+
+  return_value_if_fail(image_manager_load(image_manager(), image->image, &bitmap) == RET_OK,
+                       RET_BAD_PARAMS);
 
   if (vg != NULL) {
     bool_t need_transform = !tk_fequal(image->scale_x, 1) || !tk_fequal(image->scale_y, 1) ||
@@ -91,8 +94,8 @@ static ret_t image_on_paint_self(widget_t* widget, canvas_t* c) {
         vgcanvas_scale(vg, image->scale_x, image->scale_y);
       }
       vgcanvas_translate(vg, -anchor_x, -anchor_y);
-      if (bitmap->data != NULL) {
-        vgcanvas_draw_icon(vg, bitmap, 0, 0, bitmap->w, bitmap->h, 0, 0, widget->w, widget->h);
+      if (bitmap.data != NULL) {
+        vgcanvas_draw_icon(vg, &bitmap, 0, 0, bitmap.w, bitmap.h, 0, 0, widget->w, widget->h);
       }
       vgcanvas_restore(vg);
 
@@ -100,7 +103,7 @@ static ret_t image_on_paint_self(widget_t* widget, canvas_t* c) {
     }
   }
 
-  if (bitmap->data != NULL) {
+  if (bitmap.data != NULL) {
     color_t trans = color_init(0x00, 0x00, 0x00, 0x00);
     color_t bg = style_get_color(&(widget->style_data), STYLE_ID_BG_COLOR, trans);
     color_t border = style_get_color(&(widget->style_data), STYLE_ID_BORDER_COLOR, trans);
@@ -123,7 +126,7 @@ static ret_t image_on_paint_self(widget_t* widget, canvas_t* c) {
     }
 
     dst = rect_init(0, 0, widget->w, widget->h);
-    canvas_draw_image_ex(c, bitmap, image->draw_type, &dst);
+    canvas_draw_image_ex(c, &bitmap, image->draw_type, &dst);
   }
 
   widget_paint_helper(widget, c, NULL, NULL);
@@ -136,12 +139,10 @@ static ret_t image_get_prop(widget_t* widget, const char* name, value_t* v) {
   return_value_if_fail(widget != NULL && name != NULL && v != NULL, RET_BAD_PARAMS);
 
   if (tk_str_eq(name, WIDGET_PROP_IMAGE)) {
-    value_set_str(v, image->bitmap.name);
+    value_set_str(v, image->image);
     return RET_OK;
   } else if (tk_str_eq(name, WIDGET_PROP_DRAW_TYPE)) {
-    const key_type_value_t* kv = image_draw_type_find_by_value(image->draw_type);
-    const char* draw_type = kv != NULL ? kv->name : "default";
-    value_set_str(v, draw_type);
+    value_set_int(v, image->draw_type);
     return RET_OK;
   } else if (tk_str_eq(name, WIDGET_PROP_SCALE_X)) {
     value_set_float(v, image->scale_x);
@@ -174,11 +175,7 @@ static ret_t image_set_prop(widget_t* widget, const char* name, const value_t* v
   return_value_if_fail(widget != NULL && name != NULL && v != NULL, RET_BAD_PARAMS);
 
   if (tk_str_eq(name, WIDGET_PROP_IMAGE)) {
-    if (v->type == VALUE_TYPE_STRING) {
-      return image_set_image_name(widget, value_str(v));
-    } else if (v->type == VALUE_TYPE_POINTER) {
-      return image_set_image(widget, (bitmap_t*)value_pointer(v));
-    }
+    return image_set_image(widget, value_str(v));
   } else if (tk_str_eq(name, WIDGET_PROP_DRAW_TYPE)) {
     if (v->type == VALUE_TYPE_STRING) {
       const key_type_value_t* kv = image_draw_type_find(value_str(v));
@@ -216,6 +213,15 @@ static ret_t image_set_prop(widget_t* widget, const char* name, const value_t* v
   return RET_NOT_FOUND;
 }
 
+static ret_t image_destroy(widget_t* widget) {
+  image_t* image = IMAGE(widget);
+  return_value_if_fail(image != NULL, RET_BAD_PARAMS);
+
+  TKMEM_FREE(image->image);
+
+  return RET_OK;
+}
+
 static const char* s_image_clone_properties[] = {WIDGET_PROP_IMAGE,      WIDGET_PROP_DRAW_TYPE,
                                                  WIDGET_PROP_SCALE_X,    WIDGET_PROP_SCALE_Y,
                                                  WIDGET_PROP_ANCHOR_X,   WIDGET_PROP_ANCHOR_Y,
@@ -225,6 +231,7 @@ static const widget_vtable_t s_image_vtable = {.size = sizeof(image_t),
                                                .type = WIDGET_TYPE_IMAGE,
                                                .clone_properties = s_image_clone_properties,
                                                .create = image_create,
+                                               .destroy = image_destroy,
                                                .on_event = image_on_event,
                                                .on_paint_self = image_on_paint_self,
                                                .set_prop = image_set_prop,
@@ -247,24 +254,12 @@ widget_t* image_create(widget_t* parent, xy_t x, xy_t y, wh_t w, wh_t h) {
   return widget;
 }
 
-ret_t image_set_image_name(widget_t* widget, const char* name) {
-  bitmap_t bitmap;
-  return_value_if_fail(widget != NULL && name != NULL, RET_BAD_PARAMS);
-  return_value_if_fail(image_manager_load(image_manager(), name, &bitmap) == RET_OK,
-                       RET_BAD_PARAMS);
-
-  return image_set_image(widget, &bitmap);
-}
-
-static ret_t image_set_image(widget_t* widget, bitmap_t* bitmap) {
+ret_t image_set_image(widget_t* widget, const char* name) {
   image_t* image = IMAGE(widget);
-  return_value_if_fail(widget != NULL && bitmap != NULL, RET_BAD_PARAMS);
+  return_value_if_fail(widget != NULL && name != NULL, RET_BAD_PARAMS);
 
-  if (bitmap != NULL) {
-    image->bitmap = *bitmap;
-  } else {
-    memset(&image->bitmap, 0x00, sizeof(bitmap_t));
-  }
+  TKMEM_FREE(image->image);
+  image->image = tk_strdup(name);
 
   return RET_OK;
 }
