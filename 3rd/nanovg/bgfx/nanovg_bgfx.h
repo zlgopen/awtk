f0673cd7dc2e15deb482984eae49196b0a7517ba
xianjimli <xianjimli@gmail.com>
2018-11-15 08:58:18 +0800

    sync bgfx code

--- 3rd/nanovg/bgfx/nanovg_bgfx.h
+++ 3rd/nanovg/bgfx/nanovg_bgfx.h
@@ -17,6 +17,14 @@
 #define M_PI 3.1415926f
 #endif /*M_PI*/
 
+typedef struct _NVGLUframebuffer_bgfx
+{
+  NVGcontext* ctx;
+  uint16_t handle;
+  int image;
+  uint16_t viewId;
+} NVGLUframebuffer_bgfx ;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -36,6 +44,41 @@ void touchBGFX(uint16_t _viewId);
 uint32_t frameBGFX(bool _capture);
 
 void resetBGFX(uint32_t _width, uint32_t _height, uint32_t _flags);
+
+// Helper functions to create bgfx framebuffer to render to.
+// Example:
+//		float scale = 2;
+//		NVGLUframebuffer_bgfx* fb = nvgluCreateFramebuffer(ctx, 100 * scale, 100 * scale, 0);
+//		nvgluSetViewFramebuffer(VIEW_ID, fb);
+//		nvgluBindFramebuffer(fb);
+//		nvgBeginFrame(ctx, 100, 100, scale);
+//		// renders anything offscreen
+//		nvgEndFrame(ctx);
+//		nvgluBindFramebuffer(NULL);
+//
+//		// Pastes the framebuffer rendering.
+//		nvgBeginFrame(ctx, 1024, 768, scale);
+//		NVGpaint paint = nvgImagePattern(ctx, 0, 0, 100, 100, 0, fb->image, 1);
+//		nvgBeginPath(ctx);
+//		nvgRect(ctx, 0, 0, 100, 100);
+//		nvgFillPaint(ctx, paint);
+//		nvgFill(ctx);
+//		nvgEndFrame(ctx);
+
+///
+NVGLUframebuffer_bgfx* nvgluCreateFramebufferByViewId(NVGcontext* _ctx, int32_t _width, int32_t _height, int32_t _imageFlags, uint16_t _viewId);
+
+///
+NVGLUframebuffer_bgfx* nvgluCreateFramebuffer(NVGcontext* _ctx, int32_t _width, int32_t _height, int32_t _imageFlags);
+
+///
+void nvgluBindFramebuffer(NVGLUframebuffer_bgfx* _framebuffer_bgfx);
+
+///
+void nvgluDeleteFramebuffer(NVGLUframebuffer_bgfx* _framebuffer_bgfx);
+
+///
+void nvgluSetViewFramebuffer(uint16_t _viewId, NVGLUframebuffer_bgfx* _framebuffer_bgfx);
 
 #ifdef __cplusplus
 }
