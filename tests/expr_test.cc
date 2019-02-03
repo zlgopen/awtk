734164bdd63f5b68dbc9749d0e4e183282f31b8e
xianjimli <xianjimli@gmail.com>
2019-02-04 07:58:13 +0800

    update assets and add tests

--- tests/expr_test.cc
+++ tests/expr_test.cc
@@ -7,4 +7,6 @@ TEST(ExprEval, basic) {
   ASSERT_EQ(2, tk_expr_eval("1 * 2"));
   ASSERT_EQ(2, tk_expr_eval("4/2"));
   ASSERT_EQ(4, tk_expr_eval("(1+1)*2"));
+  ASSERT_EQ(1, tk_expr_eval("\"aa\"==\"aa\""));
+  ASSERT_EQ(0, tk_expr_eval("\"aa\"==\"bb\""));
 }
