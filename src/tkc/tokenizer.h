70f0bdd4200df5a7399435181e7a1c444afce0e0
xianjimli <xianjimli@gmail.com>
2019-02-04 18:02:51 +0800

    add tokenizer_next_until

--- src/tkc/tokenizer.h
+++ src/tkc/tokenizer.h
@@ -115,6 +115,16 @@ bool_t tokenizer_has_more(tokenizer_t* tokenizer);
 const char* tokenizer_next(tokenizer_t* tokenizer);
 
 /**
+ * @method tokenizer_next_until
+ * 获取下一个token，该token直到遇到指定的char。
+ * @param {tokenizer_t*} tokenizer tokenizer对象。
+ * @param {const char*} str 字符集。
+ *
+ * @return {char*} 成功返回token，失败返回NULL。
+ */
+const char* tokenizer_next_until(tokenizer_t* tokenizer, const char* str);
+
+/**
  * @method tokenizer_next_int
  * 获取下一个token，并转换成int。
  * @param {tokenizer_t*} tokenizer tokenizer对象。
