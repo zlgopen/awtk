3b14b9758f20fe3e12b5e76d9ec8e61309293f70
lixianjing <xianjimli@hotmail.com>
2019-05-07 17:43:11 +0800

    format code

--- src/base/image_loader.c
+++ src/base/image_loader.c
@@ -34,8 +34,8 @@ static image_loader_t* s_image_loaders[MAX_IMAGE_LOADERS];
 ret_t image_loader_register(image_loader_t* loader) {
   uint32_t i = 0;
   return_value_if_fail(loader != NULL, RET_BAD_PARAMS);
-  for(i = 0; i < ARRAY_SIZE(s_image_loaders); i++) {
-    if(s_image_loaders[i] == NULL) {
+  for (i = 0; i < ARRAY_SIZE(s_image_loaders); i++) {
+    if (s_image_loaders[i] == NULL) {
       s_image_loaders[i] = loader;
 
       return RET_OK;
@@ -48,8 +48,8 @@ ret_t image_loader_register(image_loader_t* loader) {
 ret_t image_loader_unregister(image_loader_t* loader) {
   uint32_t i = 0;
   return_value_if_fail(loader != NULL, RET_BAD_PARAMS);
-  for(i = 0; i < ARRAY_SIZE(s_image_loaders); i++) {
-    if(s_image_loaders[i] == loader) {
+  for (i = 0; i < ARRAY_SIZE(s_image_loaders); i++) {
+    if (s_image_loaders[i] == loader) {
       s_image_loaders[i] = NULL;
 
       return RET_OK;
@@ -63,15 +63,13 @@ ret_t image_loader_load_image(const asset_info_t* asset, bitmap_t* bitmap) {
   uint32_t i = 0;
   return_value_if_fail(asset != NULL && bitmap != NULL, RET_BAD_PARAMS);
 
-  for(i = 0; i < ARRAY_SIZE(s_image_loaders); i++) {
+  for (i = 0; i < ARRAY_SIZE(s_image_loaders); i++) {
     image_loader_t* loader = s_image_loaders[i];
 
-    if(loader != NULL && image_loader_load(loader, asset, bitmap) == RET_OK) {
+    if (loader != NULL && image_loader_load(loader, asset, bitmap) == RET_OK) {
       return RET_OK;
     }
   }
 
   return RET_FAIL;
 }
-
-
