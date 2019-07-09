97023065af414d579368a089e18c7669863a353b
lixianjing <xianjimli@hotmail.com>
2019-07-09 10:14:09 +0800

    improve window animation

--- src/window_animators/window_animator_common.c
+++ src/window_animators/window_animator_common.c
@@ -25,10 +25,15 @@ ret_t window_animator_to_bottom_draw_curr(window_animator_t* wa) {
   canvas_t* c = wa->canvas;
   widget_t* win = wa->curr_win;
   float_t percent = wa->percent;
-
+  int32_t y = win->y;
   int32_t h = tk_roundi(win->h * percent);
+
   rect_t src = rect_init(win->x, win->y + win->h - h, win->w, h);
-  rect_t dst = rect_init(win->x, tk_roundi(win->y * percent), win->w, h);
+  rect_t dst = rect_init(win->x, y, win->w, h);
+
+  if (h == 0) {
+    return RET_OK;
+  }
 
   return lcd_draw_image(c->lcd, &(wa->curr_img), rect_scale(&src, wa->ratio), &dst);
 }
@@ -38,10 +43,14 @@ ret_t window_animator_to_top_draw_curr(window_animator_t* wa) {
   widget_t* win = wa->curr_win;
   float_t percent = wa->percent;
   int32_t h = tk_roundi(win->h * percent);
-  int32_t y = win->parent->h - tk_roundi((win->parent->h - win->y) * percent);
+  int32_t y = win->y + tk_roundi(win->h * (1 - percent));
 
   rect_t src = rect_init(win->x, win->y, win->w, h);
   rect_t dst = rect_init(win->x, y, win->w, h);
+
+  if (h == 0) {
+    return RET_OK;
+  }
 
   return lcd_draw_image(c->lcd, &(wa->curr_img), rect_scale(&src, wa->ratio), &dst);
 }
@@ -51,10 +60,14 @@ ret_t window_animator_to_left_draw_curr(window_animator_t* wa) {
   widget_t* win = wa->curr_win;
   float_t percent = wa->percent;
   int32_t w = tk_roundi(win->w * percent);
-  int32_t x = win->parent->w - tk_roundi((win->parent->w - win->x) * percent);
+  int32_t x = win->x + tk_roundi(win->w * (1 - percent));
 
   rect_t src = rect_init(win->x, win->y, w, win->h);
   rect_t dst = rect_init(x, win->y, w, win->h);
+
+  if (w == 0) {
+    return RET_OK;
+  }
 
   return lcd_draw_image(c->lcd, &(wa->curr_img), rect_scale(&src, wa->ratio), &dst);
 }
@@ -63,10 +76,15 @@ ret_t window_animator_to_right_draw_curr(window_animator_t* wa) {
   canvas_t* c = wa->canvas;
   widget_t* win = wa->curr_win;
   float_t percent = wa->percent;
+  int32_t x = win->x;
 
   int32_t w = tk_roundi(win->w * percent);
   rect_t src = rect_init(win->x + win->w - w, win->y, w, win->h);
-  rect_t dst = rect_init(tk_roundi(win->x * percent), win->y, w, win->h);
+  rect_t dst = rect_init(x, win->y, w, win->h);
+
+  if (w == 0) {
+    return RET_OK;
+  }
 
   return lcd_draw_image(c->lcd, &(wa->curr_img), rect_scale(&src, wa->ratio), &dst);
 }
