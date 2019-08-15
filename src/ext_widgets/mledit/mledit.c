80b969ffaa81022a1746b3287723b51a3bd45de2
lixianjing <xianjimli@hotmail.com>
2019-08-16 07:14:33 +0800

    fix for awtk-web

--- src/ext_widgets/mledit/mledit.c
+++ src/ext_widgets/mledit/mledit.c
@@ -348,7 +348,7 @@ static ret_t mledit_on_event(widget_t* widget, event_t* e) {
       int32_t key = evt->key;
 
       text_edit_key_down(mledit->model, (key_event_t*)e);
-      if (key < 128 && isprint(key) || key == TK_KEY_BACKSPACE || key == TK_KEY_DELETE) {
+      if ((key < 128 && isprint(key)) || key == TK_KEY_BACKSPACE || key == TK_KEY_DELETE) {
         mledit_dispatch_event(widget, EVT_VALUE_CHANGING);
       }
 
