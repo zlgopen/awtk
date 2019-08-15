b26dfee7ab72f6acc9de4cbc22762bbd6ea0e994
lixianjing <xianjimli@hotmail.com>
2019-08-15 16:33:49 -0700

    add fragment_frame_buffer

---
+++ src/lcd/fragment_frame_buffer.h
@@ -0,0 +1,129 @@
+﻿/**
+ * File:   fragment_frame_buffer
+ * Author: AWTK Develop Team
+ * Brief:  fragment_frame_buffer
+ *
+ * Copyright (c) 2018 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * this program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * merchantability or fitness for a particular purpose.  see the
+ * license file for more details.
+ *
+ */
+
+/**
+ * history:
+ * ================================================================
+ * 2019-08-16 li xianjing <xianjimli@hotmail.com> created
+ *
+ */
+
+
+typedef struct _fragment_frame_buffer_t {
+  uint32_t x;
+  uint32_t y;
+  uint32_t w;
+  uint32_t h;
+  pixel_t* data; 
+  uint32_t pixels_nr;
+
+  rect_t win;
+  int32_t cursor_x;
+  int32_t cursor_y;
+} fragment_frame_buffer_t;
+
+static fragment_frame_buffer_t* fragment_frame_buffer_init(fragment_frame_buffer_t* ffb, pixel_t* data, uint32_t pixels_nr) {
+  return_value_if_fail(ffb != NULL && data != NULL, NULL);
+
+  memset(ffb, 0x00, sizeof(fragment_frame_buffer_t));
+
+  ffb->data = data;
+  ffb->pixels_nr = pixels_nr;
+
+  return ffb;
+}
+
+static fragment_frame_buffer_t* fragment_frame_buffer_begin_frame(fragment_frame_buffer_t* ffb, rect_t* r) {
+  uint32_t nr = 0;
+  return_value_if_fail(ffb != NULL && r != NULL, NULL);
+
+  nr = r->w * r->h;
+
+  if(nr > ffb->pixels_nr) {
+    return NULL;
+  }
+
+  ffb->x = r->x;
+  ffb->y = r->y;
+  ffb->w = r->w;
+  ffb->h = r->h;
+  
+  return ffb;
+}
+
+static inline ret_t fragment_frame_buffer_set_pixel(fragment_frame_buffer_t* ffb, 
+  uint32_t x, uint32_t y, pixel_t pixel) {
+  uint32_t dx = x - ffb->x; 
+  uint32_t dy = y - ffb->y; 
+  pixel_t* p = ffb->data + dy * ffb->w + dx;
+
+  return_value_if_fail((x >= ffb->x && y >= ffb->y), RET_BAD_PARAMS);
+  return_value_if_fail((x < (ffb->x + ffb->w) && y < (ffb->y + ffb->h)), RET_BAD_PARAMS);
+
+  *p = pixel;
+
+  return RET_OK;
+}
+
+static inline pixel_t fragment_frame_buffer_get_pixel(fragment_frame_buffer_t* ffb, 
+  uint32_t x, uint32_t y) {
+  uint32_t dx = x - ffb->x; 
+  uint32_t dy = y - ffb->y; 
+  pixel_t* p = ffb->data + dy * ffb->w + dx;
+
+  assert(x >= ffb->x && y >= ffb->y && x < (ffb->x + ffb->w) && y < (ffb->y + ffb->h));
+
+  return *p;
+}
+
+static inline ret_t fragment_frame_buffer_end_frame(fragment_frame_buffer_t* ffb) {
+  uint32_t i = 0;
+  pixel_t* p = ffb->data;
+  uint32_t nr = ffb->w * ffb->h;
+
+  set_window_func(ffb->x, ffb->y, ffb->w, ffb->h);
+  for(i = 0; i < nr; i++, p++) {
+    write_data_func(*p);
+  }
+
+  return RET_OK;
+}
+
+static inline ret_t fragment_frame_buffer_set_window(fragment_frame_buffer_t* ffb, 
+  uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
+
+  ffb->win.x = x;
+  ffb->win.y = y;
+  ffb->win.w = w;
+  ffb->win.h = y;
+  ffb->cursor_x = 0;
+  ffb->cursor_y = 0;
+
+  return RET_OK;
+}
+
+static inline ret_t fragment_frame_buffer_write_data(fragment_frame_buffer_t* ffb, pixel_t pixel) { 
+  uint32_t x = ffb->win.x + ffb->cursor_x;
+  uint32_t y = ffb->win.x + ffb->cursor_y;
+  ret_t ret = fragment_frame_buffer_set_pixel(ffb, x, y, pixel);
+  
+  ffb->cursor_x++;
+  if(ffb->cursor_x >= ffb->win.w) {
+    ffb->cursor_x = 0;
+    ffb->cursor_y++;
+    assert(ffb->cursor_y < ffb->win.h);
+  }
+
+  return ret;
+}
---
+++ tests/fragment_frame_buffer_test.cc
@@ -0,0 +1,101 @@
+#include <string>
+#include "gtest/gtest.h"
+#include "tkc/rect.h"
+#include "tkc/utils.h"
+
+using std::string;
+
+
+#define pixel_t uint16_t
+
+static string s_log;
+static void set_window_func(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
+  char buff[128];
+  tk_snprintf(buff, sizeof(buff), "win(%u %u %u %u):", x, y, w, h);
+  s_log += buff;
+}
+
+static void write_data_func(pixel_t pixel) {
+  char buff[128];
+  tk_snprintf(buff, sizeof(buff), "%02x ", pixel);
+  s_log += buff;
+}
+
+
+#include "lcd/fragment_frame_buffer.h"
+
+TEST(FragmentFrameBuffer, basic) {
+  pixel_t data[4 * 4];
+  fragment_frame_buffer_t affb;
+  fragment_frame_buffer_init(&affb, data, ARRAY_SIZE(data));
+ 
+  rect_t r = rect_init(4, 4, 3, 2);
+
+  s_log = "";
+  fragment_frame_buffer_t* ffb = fragment_frame_buffer_begin_frame(&affb, &r);
+
+  ASSERT_EQ(ffb->x, r.x);
+  ASSERT_EQ(ffb->y, r.y);
+  ASSERT_EQ(ffb->w, r.w);
+  ASSERT_EQ(ffb->h, r.h);
+
+  fragment_frame_buffer_set_window(ffb, 4, 4, 3, 2);
+  fragment_frame_buffer_write_data(ffb, 1);
+  fragment_frame_buffer_write_data(ffb, 2);
+  fragment_frame_buffer_write_data(ffb, 3);
+  fragment_frame_buffer_write_data(ffb, 4);
+  fragment_frame_buffer_write_data(ffb, 5);
+  fragment_frame_buffer_write_data(ffb, 6);
+  fragment_frame_buffer_end_frame(ffb);
+
+  ASSERT_EQ(s_log, "win(4 4 3 2):01 02 03 04 05 06 ");
+}
+
+TEST(FragmentFrameBuffer, not_large_enough) {
+  pixel_t data[4 * 4];
+  fragment_frame_buffer_t affb;
+  fragment_frame_buffer_init(&affb, data, ARRAY_SIZE(data));
+
+  rect_t r = rect_init(4, 4, 5, 5);
+  fragment_frame_buffer_t* ffb = fragment_frame_buffer_begin_frame(&affb, &r);
+
+  ASSERT_EQ(ffb, (fragment_frame_buffer_t*)NULL);
+}
+
+TEST(FragmentFrameBuffer, set) {
+  pixel_t data[4 * 4];
+  fragment_frame_buffer_t affb;
+  fragment_frame_buffer_init(&affb, data, ARRAY_SIZE(data));
+ 
+  rect_t r = rect_init(4, 4, 3, 2);
+
+  s_log = "";
+  fragment_frame_buffer_t* ffb = fragment_frame_buffer_begin_frame(&affb, &r);
+
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 0, 0, 0), RET_BAD_PARAMS);
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 3, 3, 0), RET_BAD_PARAMS);
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 7, 3, 0), RET_BAD_PARAMS);
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 0, 6, 0), RET_BAD_PARAMS);
+
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 4, 4, 1), RET_OK);
+  ASSERT_EQ(fragment_frame_buffer_get_pixel(ffb, 4, 4), 1);
+  
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 5, 4, 2), RET_OK);
+  ASSERT_EQ(fragment_frame_buffer_get_pixel(ffb, 5, 4), 2);
+  
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 6, 4, 3), RET_OK);
+  ASSERT_EQ(fragment_frame_buffer_get_pixel(ffb, 6, 4), 3);
+  
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 4, 5, 4), RET_OK);
+  ASSERT_EQ(fragment_frame_buffer_get_pixel(ffb, 4, 5), 4);
+  
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 5, 5, 5), RET_OK);
+  ASSERT_EQ(fragment_frame_buffer_get_pixel(ffb, 5, 5), 5);
+  
+  ASSERT_EQ(fragment_frame_buffer_set_pixel(ffb, 6, 5, 6), RET_OK);
+  ASSERT_EQ(fragment_frame_buffer_get_pixel(ffb, 6, 5), 6);
+  
+  fragment_frame_buffer_end_frame(ffb);
+
+  ASSERT_EQ(s_log, "win(4 4 3 2):01 02 03 04 05 06 ");
+}
