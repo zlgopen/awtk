222f12c38cbbfa6aa40f85ec8a6a2604095a533c
lixianjing <xianjimli@hotmail.com>
2019-08-28 15:11:16 +0800

    add mime types

---
+++ src/tkc/mime_types.h
@@ -0,0 +1,131 @@
+
+/**
+ * File:   types_def.h
+ * Author: AWTK Develop Team
+ * Brief:  basic types definitions.
+ *
+ * Copyright (c) 2018 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2019-08-28 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#ifndef MIME_TYPES_H
+#define MIME_TYPES_H
+
+#define MIME_TYPE_APPLICATION_ENVOY "application/envoy"
+#define MIME_TYPE_APPLICATION_FRACTALS "application/fractals"
+#define MIME_TYPE_APPLICATION_FUTURESPLASH "application/futuresplash"
+#define MIME_TYPE_APPLICATION_HTA "application/hta"
+#define MIME_TYPE_APPLICATION_MAC_BINHEX40 "application/mac-binhex40"
+#define MIME_TYPE_APPLICATION_MSWORD "application/msword"
+#define MIME_TYPE_APPLICATION_OCTET_STREAM "application/octet-stream"
+#define MIME_TYPE_APPLICATION_ODA "application/oda"
+#define MIME_TYPE_APPLICATION_OLESCRIPT "application/olescript"
+#define MIME_TYPE_APPLICATION_PDF "application/pdf"
+#define MIME_TYPE_APPLICATION_PICS_RULES "application/pics-rules"
+#define MIME_TYPE_APPLICATION_PKCS10 "application/pkcs10"
+#define MIME_TYPE_APPLICATION_PKIX_CRL "application/pkix-crl"
+#define MIME_TYPE_APPLICATION_POSTSCRIPT "application/postscript"
+#define MIME_TYPE_APPLICATION_RTF "application/rtf"
+#define MIME_TYPE_APPLICATION_VND_MS_EXCEL "application/vnd.ms-excel"
+#define MIME_TYPE_APPLICATION_VND_MS_OUTLOOK "application/vnd.ms-outlook"
+#define MIME_TYPE_APPLICATION_VND_MS_PKICERTSTORE "application/vnd.ms-pkicertstore"
+#define MIME_TYPE_APPLICATION_VND_MS_PKISECCAT "application/vnd.ms-pkiseccat"
+#define MIME_TYPE_APPLICATION_VND_MS_PKISTL "application/vnd.ms-pkistl"
+#define MIME_TYPE_APPLICATION_VND_MS_POWERPOINT "application/vnd.ms-powerpoint"
+#define MIME_TYPE_APPLICATION_VND_MS_PROJECT "application/vnd.ms-project"
+#define MIME_TYPE_APPLICATION_VND_MS_WORKS "application/vnd.ms-works"
+#define MIME_TYPE_APPLICATION_WINHLP "application/winhlp"
+#define MIME_TYPE_APPLICATION_X_BCPIO "application/x-bcpio"
+#define MIME_TYPE_APPLICATION_X_CDF "application/x-cdf"
+#define MIME_TYPE_APPLICATION_X_COMPRESS "application/x-compress"
+#define MIME_TYPE_APPLICATION_X_COMPRESSED "application/x-compressed"
+#define MIME_TYPE_APPLICATION_X_CPIO "application/x-cpio"
+#define MIME_TYPE_APPLICATION_X_CSH "application/x-csh"
+#define MIME_TYPE_APPLICATION_X_DIRECTOR "application/x-director"
+#define MIME_TYPE_APPLICATION_X_DVI "application/x-dvi"
+#define MIME_TYPE_APPLICATION_X_GTAR "application/x-gtar"
+#define MIME_TYPE_APPLICATION_X_GZIP "application/x-gzip"
+#define MIME_TYPE_APPLICATION_X_HDF "application/x-hdf"
+#define MIME_TYPE_APPLICATION_X_IPHONE "application/x-iphone"
+#define MIME_TYPE_APPLICATION_X_JAVASCRIPT "application/x-javascript"
+#define MIME_TYPE_APPLICATION_X_LATEX "application/x-latex"
+#define MIME_TYPE_APPLICATION_X_MSACCESS "application/x-msaccess"
+#define MIME_TYPE_APPLICATION_X_MSCARDFILE "application/x-mscardfile"
+#define MIME_TYPE_APPLICATION_X_MSCLIP "application/x-msclip"
+#define MIME_TYPE_APPLICATION_X_MSDOWNLOAD "application/x-msdownload"
+#define MIME_TYPE_APPLICATION_X_MSMEDIAVIEW "application/x-msmediaview"
+#define MIME_TYPE_APPLICATION_X_MSMETAFILE "application/x-msmetafile"
+#define MIME_TYPE_APPLICATION_X_MSMONEY "application/x-msmoney"
+#define MIME_TYPE_APPLICATION_X_MSPUBLISHER "application/x-mspublisher"
+#define MIME_TYPE_APPLICATION_X_MSSCHEDULE "application/x-msschedule"
+#define MIME_TYPE_APPLICATION_X_MSTERMINAL "application/x-msterminal"
+#define MIME_TYPE_APPLICATION_X_MSWRITE "application/x-mswrite"
+#define MIME_TYPE_APPLICATION_X_NETCDF "application/x-netcdf"
+#define MIME_TYPE_APPLICATION_X_PERFMON "application/x-perfmon"
+#define MIME_TYPE_APPLICATION_X_PKCS12 "application/x-pkcs12"
+#define MIME_TYPE_APPLICATION_X_SH "application/x-sh"
+#define MIME_TYPE_APPLICATION_X_SHAR "application/x-shar"
+#define MIME_TYPE_APPLICATION_X_SHOCKWAVE_FLASH "application/x-shockwave-flash"
+#define MIME_TYPE_APPLICATION_X_STUFFIT "application/x-stuffit"
+#define MIME_TYPE_APPLICATION_X_SV4CPIO "application/x-sv4cpio"
+#define MIME_TYPE_APPLICATION_X_SV4CRC "application/x-sv4crc"
+#define MIME_TYPE_APPLICATION_X_TAR "application/x-tar"
+#define MIME_TYPE_APPLICATION_X_TCL "application/x-tcl"
+#define MIME_TYPE_APPLICATION_X_TEX "application/x-tex"
+#define MIME_TYPE_APPLICATION_X_TEXINFO "application/x-texinfo"
+#define MIME_TYPE_APPLICATION_X_TROFF "application/x-troff"
+#define MIME_TYPE_APPLICATION_X_TROFF-MAN "application/x-troff-man"
+#define MIME_TYPE_APPLICATION_X_TROFF-ME "application/x-troff-me"
+#define MIME_TYPE_APPLICATION_X_TROFF-MS "application/x-troff-ms"
+#define MIME_TYPE_APPLICATION_X_USTAR "application/x-ustar"
+#define MIME_TYPE_APPLICATION_ZIP "application/zip"
+#define MIME_TYPE_AUDIO_BASIC "audio/basic"
+#define MIME_TYPE_AUDIO_MID "audio/mid"
+#define MIME_TYPE_AUDIO_MPEG "audio/mpeg"
+#define MIME_TYPE_AUDIO_X_AIFF "audio/x-aiff"
+#define MIME_TYPE_AUDIO_X_MPEGURL "audio/x-mpegurl"
+#define MIME_TYPE_AUDIO_X_WAV "audio/x-wav"
+#define MIME_TYPE_IMAGE_BMP "image/bmp"
+#define MIME_TYPE_IMAGE_CIS_COD "image/cis-cod"
+#define MIME_TYPE_IMAGE_GIF "image/gif"
+#define MIME_TYPE_IMAGE_IEF "image/ief"
+#define MIME_TYPE_IMAGE_JPEG "image/jpeg"
+#define MIME_TYPE_IMAGE_PIPEG "image/pipeg"
+#define MIME_TYPE_IMAGE_SVG+XML "image/svg+xml"
+#define MIME_TYPE_IMAGE_TIFF "image/tiff"
+#define MIME_TYPE_IMAGE_X_CMU-RASTER "image/x-cmu-raster"
+#define MIME_TYPE_IMAGE_X_CMX "image/x-cmx"
+#define MIME_TYPE_IMAGE_X_ICON "image/x-icon"
+#define MIME_TYPE_IMAGE_X_RGB "image/x-rgb"
+#define MIME_TYPE_IMAGE_X_XBITMAP "image/x-xbitmap"
+#define MIME_TYPE_IMAGE_X_XPIXMAP "image/x-xpixmap"
+#define MIME_TYPE_IMAGE_X_XWINDOWDUMP "image/x-xwindowdump"
+#define MIME_TYPE_MESSAGE_RFC822 "message/rfc822"
+#define MIME_TYPE_TEXT_CSS "text/css"
+#define MIME_TYPE_TEXT_H323 "text/h323"
+#define MIME_TYPE_TEXT_HTML "text/html"
+#define MIME_TYPE_TEXT_IULS "text/iuls"
+#define MIME_TYPE_TEXT_PLAIN "text/plain"
+#define MIME_TYPE_TEXT_RICHTEXT "text/richtext"
+#define MIME_TYPE_TEXT_SCRIPTLET "text/scriptlet"
+#define MIME_TYPE_TEXT_WEBVIEWHTML "text/webviewhtml"
+#define MIME_TYPE_TEXT_X_COMPONENT "text/x-component"
+#define MIME_TYPE_TEXT_X_SETEXT "text/x-setext"
+#define MIME_TYPE_TEXT_X_VCARD "text/x-vcard"
+#define MIME_TYPE_VIDEO_MPEG "video/mpeg"
+#define MIME_TYPE_VIDEO_QUICKTIME "video/quicktime"
+#define MIME_TYPE_VIDEO_X_MSVIDEO "video/x-msvideo"
+
+#endif /*MIME_TYPES_H*/
