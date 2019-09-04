a9ea3e285b6afc6cd46765ab115a902575e42809
lixianjing <xianjimli@hotmail.com>
2019-09-04 00:57:35 -0700

    fix get_time_ms on windows

--- src/platforms/pc/platform.c
+++ src/platforms/pc/platform.c
@@ -86,7 +86,7 @@ uint64_t get_time_ms() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
 
-  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  return (uint64_t)(tv.tv_sec) * 1000 + (uint64_t)(tv.tv_usec) / 1000;
 }
 
 void sleep_ms(uint32_t ms) {
