ce278444f8069762c6033e48c95f657eadb1f71f
lixianjing <xianjimli@hotmail.com>
2019-10-24 18:01:17 +0800

    merge patch of issue #257

--- src/misc/new.cpp
+++ src/misc/new.cpp
@@ -4,7 +4,7 @@
 
 #ifndef HAS_STD_MALLOC
 
-void* operator new(std::size_t size) throw(std::bad_alloc) {
+void* operator new(std::size_t size) __TK_THROW_BAD_ALLOC {
   if (size >= MAX_SIZE) {
     log_debug("size is too large\n");
   }
@@ -12,7 +12,7 @@ void* operator new(std::size_t size) throw(std::bad_alloc) {
   return TKMEM_ALLOC(size);
 }
 
-void* operator new[](std::size_t size) throw(std::bad_alloc) {
+void* operator new[](std::size_t size) __TK_THROW_BAD_ALLOC {
   if (size >= MAX_SIZE) {
     log_debug("size is too large\n");
   }
--- src/misc/new.hpp
+++ src/misc/new.hpp
@@ -7,9 +7,15 @@
 
 #ifndef HAS_STD_MALLOC
 
-void* operator new(std::size_t size) throw(std::bad_alloc);
+#if __cplusplus < 201103L || defined(__ARMCC_VERSION)
+#define __TK_THROW_BAD_ALLOC throw(std::bad_alloc)
+#else
+#define __TK_THROW_BAD_ALLOC
+#endif
 
-void* operator new[](std::size_t size) throw(std::bad_alloc);
+void* operator new(std::size_t size) __TK_THROW_BAD_ALLOC;
+
+void* operator new[](std::size_t size) __TK_THROW_BAD_ALLOC;
 
 void  operator delete(void* obj) throw();
 
