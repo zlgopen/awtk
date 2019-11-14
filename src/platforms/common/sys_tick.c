685ab4ac24cd8bf8809e6df106556a518ce9000a
lixianjing <xianjimli@hotmail.com>
2019-11-14 09:23:17 +0800

    improve sys_tick

--- src/platforms/common/sys_tick.c
+++ src/platforms/common/sys_tick.c
@@ -22,17 +22,10 @@
 #include "rtos.h"
 
 static volatile uint64_t g_sys_tick;
-static bool_t s_sys_tick_enable = FALSE;
-
-void sys_tick_enable(bool_t enable) {
-  s_sys_tick_enable = enable;
-}
 
 void SysTick_Handler(void) {
-  if (s_sys_tick_enable) {
-    g_sys_tick++;
-    rtos_tick();
-  }
+  g_sys_tick++;
+  rtos_tick();
 }
 
 uint64_t get_time_ms64() {
