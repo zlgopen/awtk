ccf96d6e998e68cfd7989e57e1d20f2f72ce4a41
xianjimli <xianjimli@gmail.com>
2019-12-17 18:03:49 +0800

    add clip_view_test

---
+++ tests/clip_view_test.cc
@@ -0,0 +1,10 @@
+﻿#include "widgets/clip_view.h"
+#include "gtest/gtest.h"
+
+TEST(ClipView, cast) {
+  widget_t* w = clip_view_create(NULL, 10, 20, 30, 40);
+
+  ASSERT_EQ(w, clip_view_cast(w));
+
+  widget_destroy(w);
+}
