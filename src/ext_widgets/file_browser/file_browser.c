3ad65c89784c5686fa6dd679a8452892187616f3
lixianjing <xianjimli@hotmail.com>
2020-01-04 21:56:54 +0800

    add file browser(not work yet)

---
+++ src/ext_widgets/file_browser/file_browser.c
@@ -0,0 +1,97 @@
+﻿/**
+ * File:   file_browser.c
+ * Author: AWTK Develop Team
+ * Brief:  file_browser
+ *
+ * Copyright (c) 2020 - 2020  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2020-01-04 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "tkc/mem.h"
+#include "tkc/path.h"
+#include "file_browser.h"
+
+file_browser_t* file_browser_create(fs_t* fs) {
+  file_browser_t* fb = NULL;
+  return_value_if_fail(fs != NULL, NULL);
+  fb = TKMEM_ZALLOC(file_browser_t);
+
+  return_value_if_fail(fb != NULL, NULL);
+  fb->fs = fs;
+
+  return fb;
+}
+
+ret_t file_browser_set_cwd(file_browser_t* fb, const char* cwd) {
+  char path[MAX_PATH + 1];
+  return_value_if_fail(fb != NULL && cwd != NULL, RET_BAD_PARAMS);
+
+  memset(path, 0x00, sizeof(path));
+  path_normalize(cwd, path, MAX_PATH);
+  str_set(&(fb->cwd), path);
+
+  return RET_OK;
+}
+
+ret_t file_browser_create_dir(file_browser_t* fb, const char* name) {
+  return RET_OK;
+}
+
+ret_t file_browser_remove_dir(file_browser_t* fb, const char* name) {
+  return RET_OK;
+}
+
+ret_t file_browser_enter(file_browser_t* fb, const char* dir) {
+  return_value_if_fail(fb != NULL && dir != NULL, RET_BAD_PARAMS);
+
+  return RET_OK;
+}
+
+ret_t file_browser_up(file_browser_t* fb) {
+  return RET_OK;
+}
+
+ret_t file_browser_set_filter(file_browser_t* fb, tk_filter_t filter) {
+  return_value_if_fail(fb != NULL, RET_BAD_PARAMS);
+
+  fb->filter = filter;
+
+  return RET_OK;
+}
+
+ret_t file_browser_set_compare(file_browser_t* fb, tk_compare_t compare) {
+  return_value_if_fail(fb != NULL, RET_BAD_PARAMS);
+
+  fb->compare = compare;
+
+  return RET_OK;
+}
+
+uint32_t file_browser_get_items_nr(file_browser_t* fb) {
+  return 0;
+}
+
+filebrowser_item_t* file_browser_get_item(file_browser_t* fb, uint32_t index) {
+  return NULL;
+}
+
+ret_t file_browser_destroy(file_browser_t* fb) {
+  return_value_if_fail(fb != NULL, RET_BAD_PARAMS);
+  darray_deinit(&(fb->items));
+  str_reset(&(fb->cwd));
+  TKMEM_FREE(fb);
+
+  return RET_OK;
+}
---
+++ src/ext_widgets/file_browser/file_browser.h
@@ -0,0 +1,187 @@
+﻿/**
+ * File:   file_browser.h
+ * Author: AWTK Develop Team
+ * Brief:  file_browser
+ *
+ * Copyright (c) 2020 - 2020  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2020-01-04 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#ifndef TK_FILE_BROWSER_H
+#define TK_FILE_BROWSER_H
+
+#include "tkc/fs.h"
+#include "tkc/str.h"
+#include "tkc/darray.h"
+#include "tkc/emitter.h"
+
+BEGIN_C_DECLS
+
+typedef struct _filebrowser_item_t {
+  fs_item_t info;
+  fs_file_stat_t stat;
+} filebrowser_item_t;
+
+/**
+ * @class file_browser_t
+ * 文件浏览器模型。
+ */
+typedef struct _file_browser_t {
+  emitter_t emitter;
+  /**
+   * @property {str_t} cwd
+   * 当前路径
+   *
+   */
+  str_t cwd;
+
+  /*private*/
+
+  fs_t* fs;
+  darray_t items;
+  tk_compare_t compare;
+  tk_filter_t filter;
+} file_browser_t;
+
+/**
+ * @event {event_t} EVT_ITEMS_WILL_CHANGE
+ * 文件项目改变
+ */
+
+/**
+ * @event {progress_event_t} EVT_PROGRESS
+ * 进度改变。
+ */
+
+/**
+ * @method file_browser_create
+ * 创建file_browser对象
+ * 
+ * @annotation ["constructor"]
+ *
+ * @return {file_browser_t*} 对象。
+ */
+file_browser_t* file_browser_create(fs_t* fs);
+
+/**
+ * @method file_browser_set_cwd
+ * 设置当前文件路径。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ * @param {const char*} cwd 当前目录。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t file_browser_set_cwd(file_browser_t* fb, const char* cwd);
+
+/**
+ * @method file_browser_create_dir
+ * 在当前目录下创建指定子目录。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ * @param {const char*} name 当前目录下的子目录名。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t file_browser_create_dir(file_browser_t* fb, const char* name);
+
+/**
+ * @method file_browser_remove_dir
+ * 删除当前目录下的指定子目录。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ * @param {const char*} name 当前目录下的子目录名/文件名。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t file_browser_remove_item(file_browser_t* fb, const char* name);
+
+/**
+ * @method file_browser_enter
+ * 进入当前目录下的指定子目录。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ * @param {const char*} dir 当前目录下的子目录名。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t file_browser_enter(file_browser_t* fb, const char* dir);
+
+/**
+ * @method file_browser_up
+ * 返回上一级目录。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t file_browser_up(file_browser_t* fb);
+
+/**
+ * @method file_browser_set_filter
+ * 设置过滤函数。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ * @param {tk_filter_t} filter 过滤函数。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t file_browser_set_filter(file_browser_t* fb, tk_filter_t filter);
+
+/**
+ * @method file_browser_set_compare
+ * 设置排序用的比较函数。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ * @param {tk_compare_t} compare 排序用的比较函数。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t file_browser_set_compare(file_browser_t* fb, tk_compare_t compare);
+
+/**
+ * @method file_browser_get_items_nr
+ * 获取当前目录下文件项个数。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ *
+ * @return {uint32_t} 返回当前目录下文件项个数。
+ */
+uint32_t file_browser_get_items_nr(file_browser_t* fb);
+
+/**
+ * @method file_browser_get_item
+ * 获取指定项序数的项目。
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ * @param {uint32_t} index 序数。
+ *
+ * @return {filebrowser_item_t*} 返回指定项序数的项目。
+ */
+filebrowser_item_t* file_browser_get_item(file_browser_t* fb, uint32_t index);
+
+/**
+ * @method file_browser_destroy
+ * 销毁file_browser对象
+ * 
+ * @param {file_browser_t*} fb filebrowser对象。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t file_browser_destroy(file_browser_t* fb);
+
+END_C_DECLS
+
+#endif /*TK_FILE_BROWSER_H*/
