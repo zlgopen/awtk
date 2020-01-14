4eb51fb23ac7600a4f734b016ddd61f65defb024
lixianjing <xianjimli@hotmail.com>
2020-01-14 00:35:19 -0800

    fix test

--- tests/date_time_format_test.cc
+++ tests/date_time_format_test.cc
@@ -5,6 +5,6 @@ TEST(DateTimeFormat, basic) {
   wstr_t str;
   wstr_init(&str, 0);
   wstr_format_time(&str, "YY-MM-DD", 0);
-  ASSERT_EQ(wcscmp(str.str, L"70-01-01"), 0);
+  ASSERT_EQ(wcscmp(str.str, L"70-01-01") == 0 || wcscmp(str.str, L"69-12-31") == 0, true);
   wstr_reset(&str);
 }
