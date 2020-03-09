091d55fdc70679172811139695b0fc42f043c1ad
xianjimli <xianjimli@gmail.com>
2020-03-09 18:17:11 +0800

    add sync

---
+++ src/tkc/async.c
@@ -0,0 +1,92 @@
+﻿/**
+ * File:   async.c
+ * Author: AWTK Develop Team
+ * Brief:  async call
+ *
+ * Copyright (c) 2019 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ===============================================================
+ * 2019-05-11 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "tkc/mem.h"
+#include "tkc/utils.h"
+#include "tkc/event.h"
+
+#include "tkc/async.h"
+#include "tkc/action_thread_pool.h"
+
+static action_thread_pool_t* s_async_thread_pool = NULL;
+
+typedef struct _async_call_info_t {
+  void* ctx;
+  async_exec_t exec;
+  async_on_result_t on_result;
+} async_call_info_t;
+
+static ret_t async_call_info_init(async_call_info_t* info, async_exec_t exec,
+                                  async_on_result_t on_result, void* ctx) {
+  return_value_if_fail(info != NULL && exec != NULL, RET_BAD_PARAMS);
+
+  info->ctx = ctx;
+  info->exec = exec;
+  info->on_result = on_result;
+
+  return RET_OK;
+}
+
+static ret_t qaction_async_exec(qaction_t* action) {
+  async_call_info_t* info = (async_call_info_t*)action->args;
+
+  return info->exec(info->ctx);
+}
+
+static ret_t qaction_async_on_event(qaction_t* action, event_t* event) {
+  async_call_info_t* info = (async_call_info_t*)action->args;
+
+  if (event->type == EVT_DONE) {
+    done_event_t* e = (done_event_t*)event;
+    info->on_result(info->ctx, e->result);
+    qaction_destroy(action);
+  }
+
+  return RET_OK;
+}
+
+ret_t async_call(async_exec_t exec, async_on_result_t on_result, void* ctx) {
+  qaction_t* a = NULL;
+  async_call_info_t info;
+  return_value_if_fail(async_call_info_init(&info, exec, on_result, ctx) == RET_OK, RET_FAIL);
+
+  a = qaction_create(qaction_async_exec, &info, sizeof(info));
+  ENSURE(qaction_set_on_event(a, qaction_async_on_event) == RET_OK);
+
+  return action_thread_pool_exec(s_async_thread_pool, a);
+}
+
+ret_t async_call_init(void) {
+  return_value_if_fail(s_async_thread_pool == NULL, RET_BAD_PARAMS);
+  s_async_thread_pool = action_thread_pool_create(5, 1);
+  return_value_if_fail(s_async_thread_pool != NULL, RET_BAD_PARAMS);
+
+  return RET_OK;
+}
+
+ret_t async_call_deinit(void) {
+  return_value_if_fail(s_async_thread_pool != NULL, RET_BAD_PARAMS);
+
+  action_thread_pool_destroy(s_async_thread_pool);
+  s_async_thread_pool = NULL;
+
+  return RET_OK;
+}
---
+++ src/tkc/async.h
@@ -0,0 +1,79 @@
+﻿/**
+ * File:   async.h
+ * Author: AWTK Develop Team
+ * Brief:  async
+ *
+ * Copyright (c) 2019 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ===============================================================
+ * 2019-05-11 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#ifndef TK_ASYNC_H
+#define TK_ASYNC_H
+
+#include "tkc/types_def.h"
+
+BEGIN_C_DECLS
+
+typedef ret_t (*async_exec_t)(void* ctx);
+typedef ret_t (*async_on_result_t)(void* ctx, ret_t result);
+
+/**
+ * @class async_t
+ * 异步请求。
+ *
+ * @annotation ["fake"]
+ *
+ */
+
+/**
+ * @method async_call
+ * 异步执行exec函数，执行完成后，在后台线程调用on_result函数。
+ *
+ * @annotation ["static"]
+ *
+ * @param {async_exec_t} exec 需要异步支持的函数。
+ * @param {async_on_result_t} on_result 返回执行结果(可选)
+ * @param {void*} ctx 两个回调函数的上下文。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ *
+ */
+ret_t async_call(async_exec_t exec, async_on_result_t on_result, void* ctx);
+
+/**
+ * @method async_call_init
+ * 全局初始化。
+ *
+ * @annotation ["static"]
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ *
+ */
+ret_t async_call_init(void);
+
+/**
+ * @method async_call_deinit
+ * 全局~初始化。
+ *
+ * @annotation ["static"]
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ *
+ */
+ret_t async_call_deinit(void);
+
+END_C_DECLS
+
+#endif /*TK_ASYNC_H*/
