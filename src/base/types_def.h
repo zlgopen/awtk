f334d2513b3b3211c88d0f55c4ac75e1bf9987ed
lixianjing <xianjimli@hotmail.com>
2020-05-06 17:15:37 +0800

    define WITH_ASSET_LOADER if defined WITH_FS_RES

--- src/base/types_def.h
+++ src/base/types_def.h
@@ -449,6 +449,12 @@ typedef enum _bitmap_flag_t {
 #define BITMAP_ALIGN_SIZE 32
 #endif /*BITMAP_ALIGN_SIZE*/
 
+#ifndef WITH_ASSET_LOADER
+#ifdef WITH_FS_RES
+#define WITH_ASSET_LOADER 1
+#endif/*WITH_FS_RES*/
+#endif/*WITH_ASSET_LOADER*/
+
 struct _bitmap_t;
 typedef struct _bitmap_t bitmap_t;
 
