9e90426d8b2b926b1cc2cb8cc1bc42377d8b9f9a
xianjimli <xianjimli@gmail.com>
2020-08-08 15:48:37 +0800

    export glyph cache

--- src/base/glyph_cache.h
+++ src/base/glyph_cache.h
@@ -33,6 +33,10 @@ typedef struct _glyph_cache_item_t {
   glyph_t* g;
 } glyph_cache_item_t;
 
+/**
+ * @class glyph_cache_t
+ * glyph cache
+ */
 typedef struct _glyph_cache_t {
   uint32_t size;
   uint32_t capacity;
@@ -40,12 +44,64 @@ typedef struct _glyph_cache_t {
   tk_destroy_t destroy_glyph;
 } glyph_cache_t;
 
+/**
+ * @method glyph_cache_init
+ * 初始化glyph_cache对象。
+ * @param {glyph_cache_t*} cache cache对象。
+ * @param {uint32_t} capacity 最大容量。
+ * @param {tk_destroy_t} destroy_glyph 销毁函数。
+ * 
+ * return {glyph_cache_t*} 返回cache对象。
+ */
 glyph_cache_t* glyph_cache_init(glyph_cache_t* cache, uint32_t capacity,
                                 tk_destroy_t destroy_glyph);
+
+/**
+ * @method glyph_cache_add
+ * 增加一个glyph对象到cache。
+ * 
+ * @param {glyph_cache_t*} cache cache对象。
+ * @param {wchar_t} code 字符。
+ * @param {font_size_t} size 字体大小。
+ * @param {glyph_t} g glyph对象。
+ * 
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
 ret_t glyph_cache_add(glyph_cache_t* cache, wchar_t code, font_size_t size, glyph_t* g);
+
+/**
+ * @method glyph_cache_lookup
+ * 查找glyph对象。
+ * 
+ * @param {glyph_cache_t*} cache cache对象。
+ * @param {wchar_t} code 字符。
+ * @param {font_size_t} size 字体大小。
+ * @param {glyph_t} g glyph对象(用于返回glyph)。
+ * 
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
 ret_t glyph_cache_lookup(glyph_cache_t* cache, wchar_t code, font_size_t size, glyph_t* g);
-ret_t glyph_cache_deinit(glyph_cache_t* cache);
+
+/**
+ * @method glyph_cache_shrink
+ * 释放部分glyph。
+ * 
+ * @param {glyph_cache_t*} cache cache对象。
+ * @param {uint32_t} cache_size 保留cache个数。
+ * 
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
 ret_t glyph_cache_shrink(glyph_cache_t* cache, uint32_t cache_size);
+
+/**
+ * @method glyph_cache_deinit
+ * 释放全部cache。
+ * 
+ * @param {glyph_cache_t*} cache cache对象。
+ * 
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t glyph_cache_deinit(glyph_cache_t* cache);
 
 END_C_DECLS
 
