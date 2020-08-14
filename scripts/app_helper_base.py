f3c4c5c9cb8bd8d7d80a680777a97107e4d4f38a
xianjimli <xianjimli@gmail.com>
2020-08-14 16:43:12 +0800

    improve build scripts

---
+++ scripts/app_helper_base.py
@@ -0,0 +1,323 @@
+import os
+import sys
+import json
+import shutil
+import platform
+
+def mkdir_if_not_exist(fullpath):
+    if os.path.exists(fullpath):
+        print(fullpath+' exist.')
+    else:
+        os.makedirs(fullpath)
+
+class AppHelperBase:
+    def set_deps(self, DEPENDS_LIBS):
+        self.DEPENDS_LIBS = DEPENDS_LIBS
+        return self
+
+    def set_libs(self, APP_LIBS):
+        self.APP_LIBS = APP_LIBS
+        return self
+
+    def set_dll_def(self, DEF_FILE):
+        self.DEF_FILE = DEF_FILE
+        return self
+
+    def set_dll_def_processor(self, processor):
+        self.DEF_FILE_PROCESSOR = processor
+        return self
+
+    def set_ccflags(self, APP_CCFLAGS):
+        self.APP_CCFLAGS = APP_CCFLAGS
+        return self
+
+    def set_cxxflags(self, APP_CXXFLAGS):
+        self.APP_CXXFLAGS = APP_CXXFLAGS
+        return self
+
+    def add_deps(self, DEPENDS_LIBS):
+        self.DEPENDS_LIBS += DEPENDS_LIBS
+        return self
+
+    def add_libs(self, APP_LIBS):
+        self.APP_LIBS += APP_LIBS
+        return self
+
+    def add_cpppath(self, APP_CPPPATH):
+        self.APP_CPPPATH += APP_CPPPATH
+        return self
+
+    def add_ccflags(self, APP_CCFLAGS):
+        self.APP_CCFLAGS += APP_CCFLAGS
+        return self
+
+    def add_cxxflags(self, APP_CXXFLAGS):
+        self.APP_CXXFLAGS += APP_CXXFLAGS
+        return self
+
+    def add_linkflags(self, APP_LINKFLAGS):
+        self.APP_LINKFLAGS += APP_LINKFLAGS
+        return self
+
+    def __init__(self, ARGUMENTS):
+        APP_ROOT = os.path.normpath(os.getcwd())
+
+        self.APP_LIBS = []
+        self.DEF_FILE = None
+        self.DEF_FILE_PROCESSOR = None
+        self.DEPENDS_LIBS = []
+        self.APP_ROOT = APP_ROOT
+        self.GEN_IDL_DEF = True
+        self.BUILD_SHARED = True
+        self.ARGUMENTS = ARGUMENTS
+        self.LINUX_FB = ARGUMENTS.get('LINUX_FB', '') != ''
+        self.AWTK_ROOT = self.getAwtkRoot()
+        self.APP_BIN_DIR = os.path.join(APP_ROOT, 'bin')
+        self.APP_LIB_DIR = os.path.join(APP_ROOT, 'lib')
+        self.APP_SRC = os.path.join(APP_ROOT, 'src')
+        
+        mkdir_if_not_exist(self.APP_BIN_DIR);
+        mkdir_if_not_exist(self.APP_LIB_DIR);
+
+        sys.path.insert(0, self.AWTK_ROOT)
+        import awtk_config as awtk
+
+        self.awtk = awtk
+        self.AWTK_LIBS = awtk.LIBS
+        self.AWTK_SHARED_LIBS = awtk.SHARED_LIBS
+
+        os.environ['APP_SRC'] = self.APP_SRC
+        os.environ['APP_ROOT'] = self.APP_ROOT
+        os.environ['BIN_DIR'] = self.APP_BIN_DIR
+        os.environ['LIB_DIR'] = self.APP_LIB_DIR
+
+        self.parseArgs(awtk, ARGUMENTS)
+
+        print("AWTK_ROOT:" + self.AWTK_ROOT)
+        print(ARGUMENTS)
+
+    def saveUsesSdkInfo(self):
+        awtk = self.awtk
+        release_id = ''
+        filename = os.path.join(self.AWTK_ROOT, 'component.json')
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                component_content = f.read()
+                if len(component_content) > 0:
+                    component_json = json.loads(component_content)
+                    if 'release_id' in component_json:
+                        release_id = component_json['release_id']
+                        if sys.version_info < (3, 0):
+                            release_id = release_id.encode('ascii')
+
+        content = '{\n'
+        content += '  "compileSDK": {\n'
+        content += '    "awtk": {\n'
+        content += '      "path": "' + \
+            self.AWTK_ROOT.replace('\\', '/') + '",\n'
+        content += '      "release_id": "' + release_id + '",\n'
+        content += '      "nanovg_backend": "' + awtk.NANOVG_BACKEND + '"\n'
+        content += '    }\n'
+        content += '  }\n'
+        content += '}'
+
+        filename = os.path.join(self.APP_BIN_DIR, 'uses_sdk.json')
+        if sys.version_info < (3, 0):
+            with open(filename, 'w') as f:
+                f.write(content)
+        else:
+            with open(filename, 'w', encoding='utf8') as f:
+                f.write(content)
+
+    def isBuildShared(self):
+        return 'WITH_AWTK_SO' in os.environ and os.environ['WITH_AWTK_SO'] == 'true' and self.BUILD_SHARED 
+
+    def copyAwtkSharedLib(self):
+        self.awtk.copySharedLib(self.AWTK_ROOT, self.APP_BIN_DIR, 'awtk')
+        for iter in self.DEPENDS_LIBS:
+            for so in iter['shared_libs']:
+                self.awtk.copySharedLib(iter['root'], self.APP_BIN_DIR, so)
+
+    def genIdlAndDef(self):
+        if self.DEF_FILE:
+            print(self.DEF_FILE)
+        else:
+            return
+
+        idl_gen_tools = os.path.join(self.AWTK_ROOT, 'tools/idl_gen/index.js')
+        dll_def_gen_tools = os.path.join(
+            self.AWTK_ROOT, 'tools/dll_def_gen/index.js')
+
+        cmd = 'node ' + '"' + idl_gen_tools + '"' + ' idl/idl.json ' + 'src'
+        if os.system(cmd) != 0:
+            print('exe cmd: ' + cmd + ' failed.')
+
+        cmd = 'node ' + '"' + dll_def_gen_tools + '"' + \
+            ' idl/idl.json ' + self.DEF_FILE
+        if os.system(cmd) != 0:
+            print('exe cmd: ' + cmd + ' failed.')
+        else:
+            if self.DEF_FILE_PROCESSOR != None:
+                self.DEF_FILE_PROCESSOR()
+
+    def showHelp(self):
+        print('Options default values:')
+        print('  LCD=320_480')
+        print('  LANGUAGE=zh_CH')
+        print('  FONT="default"')
+        print('  THEME="default"')
+        print('  SHARED=true')
+        print('  LINUX_FB=false')
+        sys.exit(0)
+
+    def parseArgs(self, awtk, ARGUMENTS):
+        BUILD_SHARED = True
+        GEN_IDL_DEF = True
+        LCD_WIDTH = '320'
+        LCD_HEIGHT = '480'
+        APP_DEFAULT_FONT = 'default'
+        APP_THEME = 'default'
+        APP_RES_ROOT = '../res'
+        APP_DEFAULT_LANGUAGE = 'zh'
+        APP_DEFAULT_COUNTRY = 'CN'
+
+        if ARGUMENTS.get('HELP', ''):
+            self.showHelp()
+
+        LCD = ARGUMENTS.get('LCD', '')
+        if len(LCD) > 0:
+            wh = LCD.split('_')
+            if len(wh) >= 1:
+                LCD_WIDTH = wh[0]
+            if len(wh) >= 2:
+                LCD_HEIGHT = wh[1]
+
+        FONT = ARGUMENTS.get('FONT', '')
+        if len(FONT) > 0:
+            APP_DEFAULT_FONT = FONT
+
+        THEME = ARGUMENTS.get('THEME', '')
+        if len(THEME) > 0:
+            APP_THEME = THEME
+
+        LANGUAGE = ARGUMENTS.get('LANGUAGE', '')
+        if len(LANGUAGE) > 0:
+            lan = LANGUAGE.split('_')
+            if len(lan) >= 1:
+                APP_DEFAULT_LANGUAGE = lan[0]
+            if len(lan) >= 2:
+                APP_DEFAULT_COUNTRY = lan[1]
+
+        SHARED = ARGUMENTS.get('SHARED', '')
+        if len(SHARED) > 0 and SHARED.lower().startswith('f'):
+            self.BUILD_SHARED = False
+
+        IDL_DEF = ARGUMENTS.get('IDL_DEF', '')
+        if len(IDL_DEF) > 0 and IDL_DEF.lower().startswith('f'):
+            GEN_IDL_DEF = False
+
+        APP_CCFLAGS = ' -DLCD_WIDTH=' + LCD_WIDTH + ' -DLCD_HEIGHT=' + LCD_HEIGHT + ' '
+        APP_CCFLAGS = APP_CCFLAGS + ' -DAPP_DEFAULT_FONT=\\\"' + APP_DEFAULT_FONT + '\\\" '
+        APP_CCFLAGS = APP_CCFLAGS + ' -DAPP_THEME=\\\"' + APP_THEME + '\\\" '
+        APP_CCFLAGS = APP_CCFLAGS + ' -DAPP_RES_ROOT=\"\\\"' + APP_RES_ROOT + '\\\"\" '
+        APP_CCFLAGS = APP_CCFLAGS + ' -DAPP_DEFAULT_LANGUAGE=\\\"' + \
+            APP_DEFAULT_LANGUAGE + '\\\" '
+        APP_CCFLAGS = APP_CCFLAGS + ' -DAPP_DEFAULT_COUNTRY=\\\"' + \
+            APP_DEFAULT_COUNTRY + '\\\" '
+        APP_CCFLAGS = APP_CCFLAGS + ' -DAPP_ROOT=\"\\\"' + \
+            self.APP_ROOT + '\\\"\" '
+        os.environ['BUILD_SHARED'] = str(self.isBuildShared())
+
+        APP_LINKFLAGS = ''
+        AWTK_LIBS = self.AWTK_LIBS
+        APP_CPPPATH = [self.APP_SRC]
+        APP_LIBPATH = [self.APP_LIB_DIR]
+        AWTK_CCFLAGS = awtk.CCFLAGS
+
+        if platform.system() == 'Linux':
+            APP_LINKFLAGS += ' -Wl,-rpath=' + self.APP_BIN_DIR + ' '
+
+        if not self.isBuildShared() and hasattr(awtk, 'AWTK_CCFLAGS'):
+            AWTK_CCFLAGS = awtk.AWTK_CCFLAGS
+
+        if self.isBuildShared():
+            APP_LIBPATH = [self.APP_BIN_DIR, self.APP_LIB_DIR]
+            AWTK_LIBS = self.AWTK_SHARED_LIBS
+
+        APP_TOOLS = None
+        if hasattr(awtk, 'TOOLS_NAME') and awtk.TOOLS_NAME != '':
+            APP_TOOLS = [awtk.TOOLS_NAME]
+
+        self.AWTK_LIBS = AWTK_LIBS
+        self.AWTK_CCFLAGS = AWTK_CCFLAGS
+        self.APP_TOOLS = APP_TOOLS
+        self.APP_CCFLAGS = APP_CCFLAGS
+        self.APP_LIBPATH = APP_LIBPATH
+        self.APP_CPPPATH = APP_CPPPATH
+        self.APP_LINKFLAGS = APP_LINKFLAGS
+        self.GEN_IDL_DEF = GEN_IDL_DEF
+        self.APP_CXXFLAGS = self.APP_CCFLAGS
+
+    def prepare(self):
+        if self.GEN_IDL_DEF and not self.LINUX_FB:
+            self.genIdlAndDef()
+
+        if self.isBuildShared():
+            self.copyAwtkSharedLib()
+
+        self.saveUsesSdkInfo()
+
+    def getAwtkRoot(self):
+        return os.path.abspath('../../awtk')
+
+    def call(self, DefaultEnvironment):
+        awtk = self.awtk
+
+        CPPPATH = awtk.CPPPATH + self.APP_CPPPATH
+        LINKFLAGS = awtk.LINKFLAGS + self.APP_LINKFLAGS
+        LIBS = self.APP_LIBS + self.AWTK_LIBS
+        LIBPATH = self.APP_LIBPATH + awtk.LIBPATH
+        CCFLAGS = self.APP_CCFLAGS + self.AWTK_CCFLAGS
+        TARGET_ARCH = awtk.TARGET_ARCH
+        APP_TOOLS = self.APP_TOOLS
+        CXXFLAGS = self.APP_CXXFLAGS
+
+        for iter in self.DEPENDS_LIBS:
+            if 'shared_libs' in iter:
+                LIBS += iter['shared_libs']
+            if 'static_libs' in iter:
+                LIBS += iter['static_libs']
+            CPPPATH += [os.path.join(os.path.abspath(iter['root']), 'src')]
+            LIBPATH += [os.path.join(os.path.abspath(iter['root']), 'lib')]
+            LIBPATH += [os.path.join(os.path.abspath(iter['root']), 'bin')]
+
+        self.prepare()
+        if hasattr(awtk, 'CC'):
+            DefaultEnvironment(
+                CC=awtk.CC,
+                CXX=awtk.CXX,
+                LD=awtk.LD,
+                AR=awtk.AR,
+                STRIP=awtk.STRIP,
+                TOOLS=APP_TOOLS,
+                CPPPATH=CPPPATH,
+                LINKFLAGS=LINKFLAGS,
+                LIBS=LIBS,
+                LIBPATH=LIBPATH,
+                CCFLAGS=CCFLAGS,
+                CXXFLAGS=CXXFLAGS,
+                TARGET_ARCH=awtk.TARGET_ARCH,
+                OS_SUBSYSTEM_CONSOLE=awtk.OS_SUBSYSTEM_CONSOLE,
+                OS_SUBSYSTEM_WINDOWS=awtk.OS_SUBSYSTEM_WINDOWS)
+        else:
+            DefaultEnvironment(
+                TOOLS=APP_TOOLS,
+                CPPPATH=CPPPATH,
+                LINKFLAGS=LINKFLAGS,
+                LIBS=LIBS,
+                LIBPATH=LIBPATH,
+                CCFLAGS=CCFLAGS,
+                CXXFLAGS=CXXFLAGS,
+                TARGET_ARCH=awtk.TARGET_ARCH,
+                OS_SUBSYSTEM_CONSOLE=awtk.OS_SUBSYSTEM_CONSOLE,
+                OS_SUBSYSTEM_WINDOWS=awtk.OS_SUBSYSTEM_WINDOWS)
--- scripts/update_res_app.py
+++ scripts/update_res_app.py
@@ -2,30 +2,182 @@
 
 import os
 import sys
-
-# try add AWTK_ROOT/scripts to system environment
-AWTK_ROOT = '../awtk'
-if os.path.exists(AWTK_ROOT):
-    sys.path.append(AWTK_ROOT + '/scripts')
-elif not os.path.isabs(AWTK_ROOT):
-    AWTK_ROOT = '../' + AWTK_ROOT
-    if os.path.exists(AWTK_ROOT):
-        sys.path.append(AWTK_ROOT + '/scripts')
+import json
+import collections
 
 # AWTK_ROOT/scripts/update_res_common.py
 import update_res_common as common
 
+def use_theme_config_from_project_json():
+    global DPI
+    global THEMES
+    global OUTPUT_ROOT
+    global IS_GENERATE_INC_RES
+    global IS_GENERATE_INC_BITMAP
+    global APP_THEME
+    global APP_ROOT
 
-APP_ROOT = common.getcwd()
-if APP_ROOT.endswith('scripts'):
-    APP_ROOT = os.path.dirname(APP_ROOT)
+    project_json = common.join_path(APP_ROOT, 'project.json')
+    if not os.path.exists(project_json):
+        print('project.json is not exists.')
+        return
 
-os.chdir(APP_ROOT)
+    content = common.read_file(project_json)
+    content = json.loads(content, object_pairs_hook=collections.OrderedDict)
 
-AWTK_ROOT = common.join_path(APP_ROOT, AWTK_ROOT)
-ASSETS_ROOT = common.join_path(APP_ROOT, 'assets')
-ASSET_C = common.join_path(APP_ROOT, 'src/assets.c')
-THEMES = ['default']
+    if not isinstance(content, dict):
+        return
 
-common.init(AWTK_ROOT, ASSETS_ROOT, THEMES, ASSET_C)
-common.update_res()
+    if 'assets' not in content:
+        return
+    assets = content['assets']
+
+    if 'outputDir' in assets:
+        OUTPUT_ROOT = common.to_file_system_coding(assets['outputDir'])
+        OUTPUT_ROOT = common.join_path(APP_ROOT, OUTPUT_ROOT+'/assets')
+
+    if 'activedTheme' in assets:
+        APP_THEME = assets['activedTheme']
+
+    if 'themes' not in assets:
+        return
+    
+    if len(assets['themes']) == 0:
+        return
+
+    if assets['const'] == 'resource_data':
+        IS_GENERATE_INC_BITMAP = False
+    else:
+        IS_GENERATE_INC_RES = False
+
+    if 'screenDPR' in assets:
+        DPI = assets['screenDPR']
+
+    for theme_name, theme_setting in assets['themes'].items():
+        theme_name = common.to_file_system_coding(theme_name)
+        color_format = theme_setting['lcd']['colorFormat']
+        color_depth = theme_setting['lcd']['colorDepth']
+
+        if color_format == 'MONO':
+          imagegen_options = 'mono'
+        elif color_format == 'BGR(A)':
+            if color_depth == '16bit':
+                imagegen_options = 'bgr565'
+            else:
+                imagegen_options = 'bgra'
+        else:
+            if color_depth == '16bit':
+                imagegen_options = 'rgb565'
+            else:
+                imagegen_options = 'rgba'
+
+        if IS_GENERATE_INC_BITMAP:
+            font = theme_setting['fonts']
+            config_dir = common.join_path(ASSETS_ROOT, theme_name+'/fonts/config')
+            common.remove_dir(config_dir)
+            common.make_dirs(config_dir)
+            for font_name, font_setting in theme_setting['fonts'].items():
+                for font_size, text in font_setting.items():
+                    if font_size.isdigit():
+                        font_name = common.to_file_system_coding(font_name)
+                        font_size = common.to_file_system_coding(font_size)
+                        filename = common.join_path(config_dir, font_name+'_'+font_size+'.txt')
+                        common.write_file(filename, text)
+
+        theme = {'name': theme_name, 'imagegen_options': imagegen_options, 'packaged': theme_setting['packaged']}
+        if theme_name == 'default':
+            THEMES.insert(0, theme)
+        else:
+            THEMES.append(theme)
+
+
+def use_default_theme_config():
+    global THEMES
+
+    use_theme_config_from_project_json()
+
+    if len(THEMES) == 0:
+        if os.path.isdir(ASSETS_ROOT):
+            for file in os.listdir(ASSETS_ROOT):
+                if os.path.isdir(common.join_path(ASSETS_ROOT, file)):
+                    if file == 'default':
+                        THEMES.insert(0, file)
+                    else:
+                        THEMES.append(file)
+
+
+def is_dependencies_ok():
+    dependencies = ['bsvggen', 'fontgen', 'imagegen', 'resgen', 'strgen', 'themegen', 'xml_to_ui']
+    for d in dependencies:
+      if not os.path.exists(common.join_path(TOOLS_ROOT, common.to_exe(d))):
+          print('Can\'t find ' + common.to_exe(d) + ', please build AWTK.')
+          return False
+
+    if IS_GENERATE_INC_BITMAP:
+        for t in THEMES:
+            if isinstance(t, dict):
+                print(t['imagegen_options'])
+                if 'imagegen_options' in t and t['imagegen_options'] == 'mono':
+                    if not os.path.exists(common.join_path(TOOLS_ROOT, common.to_exe('fontgen_ft'))):
+                        print('If you need to generate fonts those display on MONO LCD, please generate fontgen_ft.')
+                        return False
+
+    return True
+
+
+def on_generate_res_event():
+    if os.path.exists(common.join_path(APP_ROOT, 'scripts/update_res_generate_res_handler.py')):
+        import update_res_generate_res_handler as generate_res
+        if hasattr(generate_res, 'on_generate_res_before'):
+           common.on_generate_res_before(generate_res.on_generate_res_before)
+        if hasattr(generate_res, 'on_generate_res_after'):
+           common.on_generate_res_after(generate_res.on_generate_res_after)
+
+
+def run(awtk_root):
+    global AWTK_ROOT
+    global TOOLS_ROOT
+    global THEMES
+    global APP_ROOT
+    global ASSETS_ROOT
+    global IS_GENERATE_INC_BITMAP
+    global IS_GENERATE_INC_BITMAP 
+
+    AWTK_ROOT = awtk_root
+    APP_ROOT = common.getcwd()
+    if APP_ROOT.endswith('scripts'):
+        APP_ROOT = os.path.dirname(APP_ROOT)
+
+    os.chdir(APP_ROOT)
+
+    DPI = 'x1'
+    THEMES = []
+    APP_THEME = 'default'
+    IS_GENERATE_INC_RES = True
+    IS_GENERATE_INC_BITMAP = True
+    TOOLS_ROOT = common.join_path(AWTK_ROOT, 'bin')
+    AWTK_ROOT = common.join_path(APP_ROOT, AWTK_ROOT)
+    ASSETS_ROOT = common.join_path(APP_ROOT, 'design')
+    ASSET_C = common.join_path(APP_ROOT, 'res/assets.inc')
+    OUTPUT_ROOT = common.join_path(APP_ROOT, 'res/assets')
+
+    use_default_theme_config()
+    if not is_dependencies_ok():
+        print('For details, please read scripts/README.md.')
+    elif len(THEMES) == 0:
+        print('Not found theme.')
+        print('For details, please read scripts/README.md.')
+    else:
+        common.init(AWTK_ROOT, ASSETS_ROOT, THEMES, ASSET_C, OUTPUT_ROOT)
+        common.set_tools_dir(TOOLS_ROOT)
+        common.set_dpi(DPI)
+        common.set_app_theme(APP_THEME)
+        common.set_enable_generate_inc_res(IS_GENERATE_INC_RES)
+        common.set_enable_generate_inc_bitmap(IS_GENERATE_INC_BITMAP)
+        on_generate_res_event()
+        common.update_res()
+
+        if isinstance(THEMES[0], dict):
+            action = common.get_action()
+            if action != 'clean' and action != 'web' and action != 'json' and action != 'pinyin':
+                common.gen_res_c(False)
