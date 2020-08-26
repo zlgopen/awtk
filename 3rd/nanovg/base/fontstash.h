f672688dc522e0b0ffc6868483e9b2d76da0f429
xianjimli <xianjimli@gmail.com>
2020-08-26 11:46:10 +0800

    fix sontstash

--- 3rd/nanovg/base/fontstash.h
+++ 3rd/nanovg/base/fontstash.h
@@ -257,10 +257,10 @@ int fons__tt_getGlyphKernAdvance(FONSttFontImpl *font, int glyph1, int glyph2)
 #else
 
 #define STB_TRUETYPE_IMPLEMENTATION
-static void* fons__tmpalloc(size_t size, void* up);
-static void fons__tmpfree(void* ptr, void* up);
-#define STBTT_malloc(x,u)    fons__tmpalloc(x,u)
-#define STBTT_free(x,u)      fons__tmpfree(x,u)
+// static void* fons__tmpalloc(size_t size, void* up);
+// static void fons__tmpfree(void* ptr, void* up);
+// #define STBTT_malloc(x,u)    fons__tmpalloc(x,u)
+// #define STBTT_free(x,u)      fons__tmpfree(x,u)
 #include "stb_truetype.h"
 
 struct FONSttFontImpl {
@@ -328,7 +328,7 @@ int fons__tt_getGlyphKernAdvance(FONSttFontImpl *font, int glyph1, int glyph2)
 #endif
 
 #ifndef FONS_SCRATCH_BUF_SIZE
-#	define FONS_SCRATCH_BUF_SIZE 96000
+#	define FONS_SCRATCH_BUF_SIZE 1
 #endif
 #ifndef FONS_HASH_LUT_SIZE
 #	define FONS_HASH_LUT_SIZE 256
