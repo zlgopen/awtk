1cca663b2e9fc7022d1448fb1d680430ba805657
lixianjing <xianjimli@hotmail.com>
2020-08-30 08:52:17 +0800

    improve app_helper to read project.json

--- scripts/app_helper_base.py
+++ scripts/app_helper_base.py
@@ -175,11 +175,30 @@ class AppHelperBase:
         print('  LINUX_FB=false')
         sys.exit(0)
 
+    def load_project_json(filename):
+        try:
+            with open(filename, encoding="UTF-8") as f:
+                info = json.load(f);
+                return info 
+        except:
+            return None
+
+    def get_project_w(info):
+        return info['assets']['themes']['default']['lcd']['width']
+
+    def get_project_h(info):
+        return info['assets']['themes']['default']['lcd']['height']
+
     def parseArgs(self, awtk, ARGUMENTS):
         BUILD_SHARED = True
         GEN_IDL_DEF = True
         LCD_WIDTH = '320'
         LCD_HEIGHT = '480'
+        config = AppHelperBase.load_project_json('project.json')
+        if config and 'assets' in config:
+            LCD_WIDTH = AppHelperBase.get_project_w(config);
+            LCD_HEIGHT = AppHelperBase.get_project_h(config);
+
         APP_DEFAULT_FONT = 'default'
         APP_THEME = 'default'
         APP_RES_ROOT = '../res'
@@ -268,6 +287,7 @@ class AppHelperBase:
         self.GEN_IDL_DEF = GEN_IDL_DEF
         self.APP_CXXFLAGS = self.APP_CCFLAGS
 
+        print(LCD_WIDTH, LCD_HEIGHT)
     def prepare(self):
         if self.GEN_IDL_DEF and not self.LINUX_FB:
             self.genIdlAndDef()
