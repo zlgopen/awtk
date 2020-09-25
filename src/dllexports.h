9251c44af928f8abe30847914fbb6369493eb224
lixianjing <xianjimli@hotmail.com>
2020-09-25 18:05:01 +0800

    export sdl functions

--- src/dllexports.h
+++ src/dllexports.h
@@ -3,51 +3,109 @@
  *
  */
 
-    /**
+/**
  * @method SDL_ShowCursor
  *
  * @annotation ["global"]
  *
  */
 
-    /**
+/**
  * @method SDL_WarpMouseInWindow
  *
  * @annotation ["global"]
  *
  */
 
-    /**
+/**
  * @method SDL_GetDefaultCursor
  *
  * @annotation ["global"]
  *
  */
 
-    /**
+/**
  * @method SDL_SetCursor
  *
  * @annotation ["global"]
  *
  */
 
-    /**
+/**
  * @method SDL_FreeCursor
  *
  * @annotation ["global"]
  *
  */
 
-    /**
+/**
  * @method SDL_CreateSystemCursor
  *
  * @annotation ["global"]
  *
  */
 
-    /**
+/**
  * @method SDL_GetMouseFocus
  *
  * @annotation ["global"]
  *
  */
+
+/**
+ * @method SDL_MixAudioFormat
+ *
+ * @annotation ["global"]
+ *
+ */
+
+/**
+ * @method SDL_DequeueAudio
+ *
+ * @annotation ["global"]
+ *
+ */
+
+/**
+ * @method SDL_QueueAudio
+ *
+ * @annotation ["global"]
+ *
+ */
+
+/**
+ * @method SDL_GetQueuedAudioSize
+ *
+ * @annotation ["global"]
+ *
+ */
+
+/**
+ * @method SDL_ClearQueuedAudio
+ *
+ * @annotation ["global"]
+ *
+ */
+
+
+/**
+ * @method SDL_PauseAudioDevice
+ *
+ * @annotation ["global"]
+ *
+ */
+
+/**
+ * @method SDL_CloseAudioDevice
+ *
+ * @annotation ["global"]
+ *
+ */
+
+/**
+ * @method SDL_OpenAudioDevice
+ *
+ * @annotation ["global"]
+ *
+ */
+
