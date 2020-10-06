7977ea956c712e84d59cb77f222fa9e4d6902719
lixianjing <xianjimli@hotmail.com>
2020-10-06 22:21:55 +0800

    improve completion_gen

--- tools/completion_gen/index.js
+++ tools/completion_gen/index.js
@@ -13,8 +13,8 @@ if(inputIDL === '-h' || inputIDL === '--help') {
 }
 
 
-function genCompletion(json) {
-  widgets = json.filter((iter) => {
+function genUICompletion(json) {
+  const widgets = json.filter((iter) => {
     return iter.annotation && iter.annotation.widget;
   });
 
@@ -40,6 +40,44 @@ function genCompletion(json) {
   fs.writeFileSync("awtk_ui.json", str);
 
   console.log(str);
+}
+
+function genStyleCompletion(json) {
+  let styleIDs = null;
+  let widgetStates = null;
+
+  const widgets = json.forEach((iter) => {
+    if(iter.name === 'style_id_t') {
+      styleIDs =  iter;
+    } else if(iter.name === 'widget_state_t') { 
+      widgetStates =  iter;
+    }
+  });
+
+  let result = {}
+  result.styleIDs = styleIDs.consts.map((iter) => {
+    let item = {}
+    item.name = iter.name.replace(styleIDs.prefix, '').toLowerCase();
+    item.desc = iter.desc.split('\n')[0].trim('\r\n ');
+    return item;
+  });
+
+  result.widgetStates = widgetStates.consts.map((iter) => {
+    let item = {}
+    item.name = iter.name.replace(widgetStates.prefix, '').toLowerCase();
+    item.desc = iter.desc.split('\n')[0].trim('\r\n ');
+    return item;
+  });
+
+  let str = JSON.stringify(result, null, '\t');
+  fs.writeFileSync("awtk_style.json", str);
+
+  console.log(str);
+}
+
+function genCompletion(json) {
+//  genUICompletion(json);
+  genStyleCompletion(json);
 }
 
 genCompletion(JSON.parse(fs.readFileSync(inputIDL).toString()));
