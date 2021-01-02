58d4904a3f27d0444641fd587c6caf7b602e4d7e
lixianjing <xianjimli@hotmail.com>
2021-01-02 18:46:08 +0800

    update copyright date

--- src/tkc/fscript.h
+++ src/tkc/fscript.h
@@ -3,7 +3,7 @@
  * Author: AWTK Develop Team
  * Brief:  a simple functional script language
  *
- * Copyright (c) 2020 - 2020  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ * Copyright (c) 2020 - 2021  Guangzhou ZHIYUAN Electronics Co.,Ltd.
  *
  */
 
