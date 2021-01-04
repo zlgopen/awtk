464f7aaeb70d519e499945efe2ec7001932e8c5d
lixianjing <xianjimli@hotmail.com>
2021-01-04 17:21:18 +0800

    add tk_strtoll

--- tests/utils_test.cc
+++ tests/utils_test.cc
@@ -22,6 +22,9 @@ TEST(Utils, basic) {
   ASSERT_EQ(tk_atof("1e2"), 100);
   ASSERT_EQ(tk_watof(L"100"), 100);
 
+  ASSERT_EQ(tk_atol("0x1122334455667788"), 0x1122334455667788);
+  ASSERT_EQ(tk_atol("1122334455667788"), 1122334455667788);
+  ASSERT_EQ(tk_atol("-1122334455667788"), -1122334455667788);
   ASSERT_EQ(strcmp(tk_itoa(str, sizeof(str), tk_atoi("100")), "100"), 0);
 }
 
