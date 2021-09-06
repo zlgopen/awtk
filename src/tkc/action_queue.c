42e9af0a839055c4a1418d680245891ba1edcfa1
lixianjing <xianjimli@hotmail.com>
2021-09-06 17:11:49 +0800

    improve action_queue

--- src/tkc/action_queue.c
+++ src/tkc/action_queue.c
@@ -27,7 +27,7 @@ action_queue_t* action_queue_create(uint16_t capacity) {
   action_queue_t* q = NULL;
   return_value_if_fail(capacity > 1, NULL);
 
-  size = sizeof(action_queue_t) + (capacity - 1) * sizeof(qaction_t);
+  size = sizeof(action_queue_t) + sizeof(qaction_t*) * capacity;
   q = (action_queue_t*)TKMEM_ALLOC(size);
   return_value_if_fail(q != NULL, NULL);
 
