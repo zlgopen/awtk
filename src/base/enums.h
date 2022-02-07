72a3f3d2626baeb148b75e03d5e1f77754c0754f
lixianjing <xianjimli@hotmail.com>
2022-02-07 09:07:51 +0800

    export input_type_find

--- src/base/enums.h
+++ src/base/enums.h
@@ -37,7 +37,18 @@ enum { TYPE_INT = 0, TYPE_COLOR, TYPE_STRING, TYPE_WSTRING, TYPE_BOOL };
  * @class enum_helper
  * @annotation ["fake"]
  */
+
+/**
+ * @method input_type_find
+ *
+ * 获取输入类型值和名称的对应关系。
+ *
+ * @param {const char*} name 输入类型名。
+ *
+ * @return {const key_type_value_t*} 返回输入类型值和名称的对应关系。
+ */
 const key_type_value_t* input_type_find(const char* name);
+
 const key_type_value_t* window_closable_type_find(const char* name);
 const key_type_value_t* align_v_type_find(const char* name);
 const key_type_value_t* align_h_type_find(const char* name);
