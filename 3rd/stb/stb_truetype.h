91f73d44639a2037699a491f8180496b932acd95
lixianjing <xianjimli@hotmail.com>
2022-05-13 15:47:56 +0800

    add log for stb

--- 3rd/stb/stb_truetype.h
+++ 3rd/stb/stb_truetype.h
@@ -1435,6 +1435,9 @@ static int stbtt_InitFont_internal(stbtt_fontinfo *info, unsigned char *data, in
       switch(ttUSHORT(data+encoding_record)) {
          case STBTT_PLATFORM_ID_MICROSOFT:
             switch (ttUSHORT(data+encoding_record+2)) {
+               case STBTT_MS_EID_SYMBOL:
+                  STBTT_printf("STBTT does not support .ttf of type MS symbol!");
+                  break;
                case STBTT_MS_EID_UNICODE_BMP:
                case STBTT_MS_EID_UNICODE_FULL:
                   // MS/Unicode
