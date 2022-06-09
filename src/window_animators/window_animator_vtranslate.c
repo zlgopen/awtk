e98c0a2f88172604c6f79c4f00903b9d24d83880
lixianjing <xianjimli@hotmail.com>
2022-06-10 07:34:15 +0800

    add param prev_win_y_range

--- src/window_animators/window_animator_vtranslate.c
+++ src/window_animators/window_animator_vtranslate.c
@@ -21,6 +21,11 @@
 
 #include "window_animators/window_animator_vtranslate.h"
 
+typedef struct _window_animator_vtranslate_t {
+  window_animator_t window_animator;
+  uint32_t prev_win_y_range;
+} window_animator_vtranslate_t;
+
 static ret_t window_animator_vtranslate_update_percent(window_animator_t* wa) {
   float_t percent = 0.2f + 0.8f * wa->easing(wa->time_percent);
   if (wa->open) {
@@ -37,7 +42,9 @@ static ret_t window_animator_vtranslate_draw_prev(window_animator_t* wa) {
   widget_t* win = wa->prev_win;
   widget_t* curr_win = wa->curr_win;
   float_t percent = wa->percent;
-  float_t y = tk_roundi(curr_win->h * percent);
+  window_animator_vtranslate_t* wav = (window_animator_vtranslate_t*)wa;
+  uint32_t range = wav->prev_win_y_range ? wav->prev_win_y_range : curr_win->h;
+  float_t y = tk_roundi(range * percent);
   float_t h = win->h - y;
 
 #ifndef WITHOUT_WINDOW_ANIMATOR_CACHE
@@ -77,11 +84,18 @@ static const window_animator_vtable_t s_window_animator_vtranslate_vt = {
     .overlap = FALSE,
     .type = "vtranslate",
     .desc = "vtranslate",
-    .size = sizeof(window_animator_t),
+    .size = sizeof(window_animator_vtranslate_t),
     .update_percent = window_animator_vtranslate_update_percent,
     .draw_prev_window = window_animator_vtranslate_draw_prev,
     .draw_curr_window = window_animator_vtranslate_draw_curr};
 
 window_animator_t* window_animator_vtranslate_create(bool_t open, tk_object_t* args) {
-  return window_animator_create(open, &s_window_animator_vtranslate_vt);
+  window_animator_t* wa = window_animator_create(open, &s_window_animator_vtranslate_vt);
+  window_animator_vtranslate_t* wav = (window_animator_vtranslate_t*)wa;
+  return_value_if_fail(wa != NULL, NULL);
+
+  /*主要用于弹出软键盘，此时前面窗口移动的位置由编辑器的位置决定，而不是软键盘的高度*/
+  wav->prev_win_y_range = tk_object_get_prop_uint32(args, "prev_win_y_range", 0);
+
+  return wa;
 }
