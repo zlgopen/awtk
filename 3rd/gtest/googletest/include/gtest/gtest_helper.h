11d48577b02a97245c025d90f2a045485d7883f0
lixianjing <xianjimli@hotmail.com>
2023-09-11 17:39:49 +0800

    add ASSERT_STREQ_UNIX_FILE

--- 3rd/gtest/googletest/include/gtest/gtest_helper.h
+++ 3rd/gtest/googletest/include/gtest/gtest_helper.h
@@ -1,4 +1,5 @@
 #include "tkc/str.h"
+#include "tkc/fs.h"
 #include "gtest/gtest.h"
 
 #ifndef GTEST_HELPER_H
@@ -21,5 +22,22 @@ static inline void ASSERT_STREQ_UNIX(const char* str1, const char* str2) {
   str_reset(&s1);
   str_reset(&s2);
 }
+
+static inline void ASSERT_STREQ_UNIX_FILE(const char* filename, const char* str2) {
+  void* str1 = file_read(filename, NULL);
+
+  if (str1 == NULL) {
+    if (TK_STR_IS_EMPTY(str2)) {
+      return;
+    }
+
+    file_write(filename, str2, strlen(str2));
+    assert(str1 != NULL);
+  }
+
+  ASSERT_STREQ_UNIX((const char*)str1, str2);
+  TKMEM_FREE(str1);
+}
+
 
 #endif/*GTEST_HELPER_H*/
