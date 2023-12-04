67704d662a104437788fdf2c40e3e0517d1d5290
lixianjing <xianjimli@hotmail.com>
2023-12-04 18:09:14 +0800

    improve log

--- src/tkc/log.h
+++ src/tkc/log.h
@@ -310,10 +310,17 @@ void awtk_ios_log(const char* message, ...);
     log_impl(format, ##args);                   \
   }
 #else
+#ifdef WITH_LOG_NOTIFY
 #define log_debug(format, args...) log_notify(LOG_LEVEL_DEBUG, format, ##args)
 #define log_info(format, args...)  log_notify(LOG_LEVEL_INFO, format, ##args)
 #define log_warn(format, args...)  log_notify(LOG_LEVEL_WARN, format, ##args)
 #define log_error(format, args...) log_notify(LOG_LEVEL_ERROR, format, ##args)
+#else
+#define log_debug(format, args...)
+#define log_info(format, args...)
+#define log_warn(format, args...)
+#define log_error(format, args...) 
+#endif/*WITH_LOG_NOTIFY*/
 #endif
 
 END_C_DECLS
