6bc2e2503f338f490f2eb8af602f677b62e4b296
lixianjing <xianjimli@hotmail.com>
2024-04-15 07:43:58 +0800

    improve awtk-config.cmake for win32

--- awtk-config.cmake
+++ awtk-config.cmake
@@ -5,8 +5,16 @@ find_library(AWTK_LIBRARY NAMES awtk PATHS ${AWTK_ROOT_DIR}/bin)
 
 file(COPY  ${AWTK_LIBRARY} DESTINATION ${PROJECT_SOURCE_DIR}/bin)
 
+add_compile_definitions(WITH_FS_RES)
+add_compile_definitions(WITH_SDL)
+add_compile_definitions(HAS_STDIO)
+add_compile_definitions(HAS_STD_MALLOC)
+
 set(AWTK_LIBRARIES ${AWTK_LIBRARY})
-set(AWTK_INCLUDE_DIRS ${AWTK_ROOT_DIR}/src ${AWTK_ROOT_DIR}/3rd ${AWTK_ROOT_DIR}/src/ext_widgets ${AWTK_ROOT_DIR}/src)
-set(AWTK_DEFINITIONS "-DWITH_FS_RES=1 ")
+set(AWTK_INCLUDE_DIRS ${AWTK_ROOT_DIR}/src ${AWTK_ROOT_DIR}/3rd ${AWTK_ROOT_DIR}/src/ext_widgets ${AWTK_ROOT_DIR}/src ${AWTK_ROOT_DIR}/3rd/SDL/include)
 
+if(WIN32)
+  file(COPY  ${AWTK_ROOT_DIR}/bin/awtk.dll DESTINATION ${CMAKE_BINARY_DIR}/Debug)
+endif()
 
+#示例请参考 awtk-hello/CMakeLists.txt
