2c929ecf21cb156bfe0f30f9905380b9a6f9cf00
lixianjing <xianjimli@hotmail.com>
2024-06-30 11:30:03 +0800

    improve icon at

--- src/base/widget.c
+++ src/base/widget.c
@@ -1538,6 +1538,7 @@ ret_t widget_calc_icon_text_rect(const rect_t* ir, int32_t font_size, float_t te
         r_icon->h = img_h_r;
         r_icon->x = ir->x;
         r_icon->y = icon_at == ICON_AT_LEFT_TOP ? 0 : (ir->h - img_h_r);
+        r_text->w = ir->w - img_w_r - spacer;
         r_text->x = ir->x + img_w_r + spacer;
       }
       break;
