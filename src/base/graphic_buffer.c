862ecb5a73160a1089a23ef38fe3a4850b9339c8
lixianjing <xianjimli@hotmail.com>
2025-04-17 16:52:22 +0800

    improve graphic_buffer (fix #937)

--- src/base/graphic_buffer.c
+++ src/base/graphic_buffer.c
@@ -68,13 +68,13 @@ ret_t graphic_buffer_set_physical_width(graphic_buffer_t* buffer, uint32_t width
 }
 
 ret_t graphic_buffer_set_physical_height(graphic_buffer_t* buffer, uint32_t height) {
-  return_value_if_fail(buffer != NULL && buffer->vt != NULL && buffer->vt->get_width != NULL, 0);
+  return_value_if_fail(buffer != NULL && buffer->vt != NULL && buffer->vt->set_height != NULL, 0);
 
   return buffer->vt->set_height(buffer, height);
 }
 
 ret_t graphic_buffer_set_physical_line_length(graphic_buffer_t* buffer, uint32_t line_length) {
-  return_value_if_fail(buffer != NULL && buffer->vt != NULL && buffer->vt->get_width != NULL, 0);
+  return_value_if_fail(buffer != NULL && buffer->vt != NULL && buffer->vt->set_line_length != NULL, 0);
 
   return buffer->vt->set_line_length(buffer, line_length);
 }
