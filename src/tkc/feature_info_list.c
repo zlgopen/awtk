1fddbc92c793ce22e88cc57c0843af76fac86411
lixianjing <xianjimli@hotmail.com>
2025-08-12 11:26:55 +0800

    improve tree

---
+++ src/tkc/feature_info_list.c
@@ -0,0 +1,200 @@
+﻿/**
+ * File:   feature_info_list.c
+ * Author: AWTK Develop Team
+ * Brief:  特征信息列表
+ *
+ * Copyright (c) 2025 - 2025 Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2025-08-08 Shen ZhaoKun <shenzhaokun@zlg.cn> created
+ *
+ */
+
+#include "tkc/feature_info_list.h"
+
+#include "tkc/mem.h"
+
+inline static int feature_info_list_item_compare_by_info(
+    const feature_info_list_item_t* iter, const feature_info_list_feature_info_t* ctx) {
+  return_value_if_fail(iter != NULL, 1);
+  return_value_if_fail(ctx != NULL, -1);
+  return iter->info - ctx;
+}
+
+inline static int feature_info_list_item_compare(const feature_info_list_item_t* iter,
+                                                 const feature_info_list_item_t* ctx) {
+  return_value_if_fail(ctx != NULL, -1);
+  return feature_info_list_item_compare_by_info(iter, ctx->info);
+}
+
+inline static ret_t feature_info_list_item_destroy(feature_info_list_item_t* item) {
+  return_value_if_fail(item != NULL, RET_BAD_PARAMS);
+  TKMEM_FREE(item);
+  return RET_OK;
+}
+
+inline static feature_info_list_item_t* feature_info_list_item_create(
+    const feature_info_list_feature_info_t* info) {
+  feature_info_list_item_t* ret = TKMEM_ZALLOC(feature_info_list_item_t);
+  return_value_if_fail(ret != NULL, NULL);
+
+  ret->info = info;
+
+  return ret;
+}
+
+feature_info_list_t* feature_info_list_create(void) {
+  feature_info_list_t* ret = TKMEM_ZALLOC(feature_info_list_t);
+  return_value_if_fail(ret != NULL, NULL);
+
+  goto_error_if_fail(NULL != slist_init(&(ret->base), (tk_destroy_t)feature_info_list_item_destroy,
+                                        (tk_compare_t)feature_info_list_item_compare));
+
+  return ret;
+error:
+  feature_info_list_destroy(ret);
+  return NULL;
+}
+
+typedef struct _feature_info_list_update_on_visit_ctx_t {
+  feature_info_list_t* list;
+  uint32_t base_offset;
+} feature_info_list_update_on_visit_ctx_t;
+
+inline static ret_t feature_info_list_update_on_visit(void* ctx, const void* data) {
+  feature_info_list_item_t* iter = (feature_info_list_item_t*)(data);
+  feature_info_list_update_on_visit_ctx_t* actx = (feature_info_list_update_on_visit_ctx_t*)(ctx);
+  return_value_if_fail(actx != NULL && iter != NULL, RET_BAD_PARAMS);
+  return_value_if_fail(actx->list != NULL, RET_BAD_PARAMS);
+
+  iter->offset = actx->base_offset + actx->list->features_size;
+  actx->list->features_size += iter->info->size;
+
+  return RET_OK;
+}
+
+inline static ret_t feature_info_list_update(feature_info_list_t* list, uint32_t base_offset) {
+  feature_info_list_update_on_visit_ctx_t ctx = {.list = list, .base_offset = base_offset};
+  return_value_if_fail(list != NULL, RET_BAD_PARAMS);
+
+  list->features_size = 0;
+  return slist_foreach(&(list->base), feature_info_list_update_on_visit, &ctx);
+}
+
+feature_info_list_item_t* feature_info_list_find(feature_info_list_t* list,
+                                                 const feature_info_list_feature_info_t* info) {
+  return_value_if_fail(list != NULL && info != NULL, NULL);
+
+  return (feature_info_list_item_t*)slist_find_ex(
+      &(list->base), (tk_compare_t)feature_info_list_item_compare_by_info, (void*)info);
+}
+
+ret_t feature_info_list_append(feature_info_list_t* list,
+                               const feature_info_list_feature_info_t* info) {
+  ret_t ret = RET_OK;
+  feature_info_list_item_t* item = NULL;
+  return_value_if_fail(list != NULL && info != NULL, RET_BAD_PARAMS);
+  return_value_if_fail(NULL == list->owner, RET_FAIL);
+
+  return_value_if_fail(NULL == feature_info_list_find(list, info), RET_FOUND);
+
+  item = feature_info_list_item_create(info);
+  return_value_if_fail(item != NULL, RET_OOM);
+
+  ret = slist_append(&(list->base), item);
+  goto_error_if_fail(RET_OK == ret);
+
+  return ret;
+error:
+  feature_info_list_item_destroy(item);
+  return ret;
+}
+
+ret_t feature_info_list_remove(feature_info_list_t* list,
+                               const feature_info_list_feature_info_t* info) {
+  return_value_if_fail(list != NULL && info != NULL, RET_BAD_PARAMS);
+  return_value_if_fail(NULL == list->owner, RET_FAIL);
+
+  return slist_remove_ex(&(list->base), (tk_compare_t)feature_info_list_item_compare_by_info,
+                         (void*)info, 1);
+}
+
+ret_t feature_info_list_clear(feature_info_list_t* list) {
+  return_value_if_fail(list != NULL, RET_BAD_PARAMS);
+  return_value_if_fail(NULL == list->owner, RET_FAIL);
+
+  return slist_remove_all(&(list->base));
+}
+
+ret_t feature_info_list_destroy(feature_info_list_t* list) {
+  return_value_if_fail(list != NULL, RET_BAD_PARAMS);
+  return_value_if_fail(NULL == list->owner, RET_FAIL);
+
+  slist_deinit(&(list->base));
+  TKMEM_FREE(list);
+
+  return RET_OK;
+}
+
+ret_t feature_info_list_attach(feature_info_list_t* list, void* owner, uint32_t attach_obj_size) {
+  return_value_if_fail(list != NULL && owner != NULL, RET_BAD_PARAMS);
+  return_value_if_fail(NULL == list->owner, RET_FAIL);
+
+  list->owner = owner;
+
+  feature_info_list_update(list, attach_obj_size);
+
+  return RET_OK;
+}
+
+ret_t feature_info_list_detach(feature_info_list_t* list) {
+  return_value_if_fail(list != NULL, RET_BAD_PARAMS);
+  ENSURE(list->owner != NULL);
+
+  list->owner = NULL;
+
+  return RET_OK;
+}
+
+static ret_t feature_info_list_init_features_on_visit(void* ctx, const void* data) {
+  const feature_info_list_item_t* item = (const feature_info_list_item_t*)(data);
+  return_value_if_fail(ctx != NULL && item != NULL, RET_BAD_PARAMS);
+
+  if (item->info->init != NULL) {
+    item->info->init(FEATURE_INFO_LIST_ITEM_GET_FEATURE(item, ctx));
+  }
+
+  return RET_OK;
+}
+
+ret_t feature_info_list_init_features(feature_info_list_t* list, void* p) {
+  return_value_if_fail(list != NULL && p != NULL, RET_BAD_PARAMS);
+
+  return slist_foreach(&(list->base), feature_info_list_init_features_on_visit, p);
+}
+
+static ret_t feature_info_list_deinit_features_on_visit(void* ctx, const void* data) {
+  const feature_info_list_item_t* item = (const feature_info_list_item_t*)(data);
+  return_value_if_fail(ctx != NULL && item != NULL, RET_BAD_PARAMS);
+
+  if (item->info->deinit != NULL) {
+    item->info->deinit(FEATURE_INFO_LIST_ITEM_GET_FEATURE(item, ctx));
+  }
+
+  return RET_OK;
+}
+
+ret_t feature_info_list_deinit_features(feature_info_list_t* list, void* p) {
+  return_value_if_fail(list != NULL && p != NULL, RET_BAD_PARAMS);
+
+  return slist_foreach(&(list->base), feature_info_list_deinit_features_on_visit, p);
+}
---
+++ src/tkc/feature_info_list.h
@@ -0,0 +1,170 @@
+﻿/**
+ * File:   feature_info_list.h
+ * Author: AWTK Develop Team
+ * Brief:  特征信息列表
+ *
+ * Copyright (c) 2025 - 2025 Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2025-08-08 Shen ZhaoKun <shenzhaokun@zlg.cn> created
+ *
+ */
+
+#ifndef TK_FEATURE_INFO_LIST_H
+#define TK_FEATURE_INFO_LIST_H
+
+#include "tkc/types_def.h"
+#include "tkc/slist.h"
+
+BEGIN_C_DECLS
+
+typedef ret_t (*feature_info_list_feature_init_t)(void* feature);
+typedef ret_t (*feature_info_list_feature_deinit_t)(void* feature);
+
+/**
+ * @struct feature_info_list_feature_info_t
+ * 特征信息
+ */
+typedef struct _feature_info_list_feature_info_t {
+  uint32_t size;
+  feature_info_list_feature_init_t init;
+  feature_info_list_feature_deinit_t deinit;
+} feature_info_list_feature_info_t;
+
+/**
+ * @struct feature_info_list_item_t
+ * 特征信息列表项
+ */
+typedef struct _feature_info_list_item_t {
+  const feature_info_list_feature_info_t* info;
+  uint32_t offset;
+} feature_info_list_item_t;
+
+/**
+ * @class feature_info_list_t
+ * @parent slist_t
+ * 特征信息列表
+ */
+typedef struct _feature_info_list_t {
+  slist_t base;
+  uint32_t features_size;
+  void* owner;
+} feature_info_list_t;
+
+/**
+ * @method feature_info_list_create
+ * @annotation ["constructor"]
+ * 创建特征信息列表
+ * @return {feature_info_list_t*} 返回特征信息列表对象
+ */
+feature_info_list_t* feature_info_list_create(void);
+
+/**
+ * @method feature_info_list_find
+ * 查找特征信息
+ * @param {tree_node_feature_info_list_t*} list 特征信息列表对象
+ * @param {const feature_info_list_feature_info_t*} info 特征信息
+ *
+ * @return {tree_node_feature_info_list_item_t*} 返回特征信息列表项对象
+ */
+feature_info_list_item_t* feature_info_list_find(feature_info_list_t* list,
+                                                 const feature_info_list_feature_info_t* info);
+
+/**
+ * @method feature_info_list_append
+ * 追加特征信息
+ * @param {feature_info_list_t*} list 特征信息列表对象
+ * @param {const feature_info_list_feature_info_t*} info 特征信息
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t feature_info_list_append(feature_info_list_t* list,
+                               const feature_info_list_feature_info_t* info);
+/**
+ * @method feature_info_list_remove
+ * 移除特征信息
+ * @param {feature_info_list_t*} list 特征信息列表对象
+ * @param {const feature_info_list_feature_info_t*} info 特征信息
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t feature_info_list_remove(feature_info_list_t* list,
+                               const feature_info_list_feature_info_t* info);
+
+/**
+ * @method feature_info_list_clear
+ * 清空特征信息列表
+ * @param {feature_info_list_t*} list 特征信息列表对象
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t feature_info_list_clear(feature_info_list_t* list);
+
+/**
+ * @method feature_info_list_destroy
+ * @annotation ["deconstructor"]
+ * 销毁特征信息列表
+ * @param {feature_info_list_t*} list 特征信息列表对象
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t feature_info_list_destroy(feature_info_list_t* list);
+
+/**
+ * @method feature_info_list_attach
+ * 绑定特征信息列表
+ * > 需要增加特征的类内部使用，外部不应该调用此接口。
+ * @param {feature_info_list_t*} list 特征信息列表对象
+ * @param {void*} owner 拥有者
+ * @param {uint32_t} attach_obj_size 附加对象的大小
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t feature_info_list_attach(feature_info_list_t* list, void* owner, uint32_t attach_obj_size);
+
+/**
+ * @method feature_info_list_detach
+ * 解绑特征信息列表
+ * > 需要增加特征的类内部使用，外部不应该调用此接口。
+ * @param {feature_info_list_t*} list 特征信息列表对象
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t feature_info_list_detach(feature_info_list_t* list);
+
+/**
+ * @method feature_info_list_init_features
+ * 初始化对象的特征
+ * > 需要增加特征的类内部使用，外部不应该调用此接口。
+ * @param {feature_info_list_t*} list 特征信息列表对象
+ * @param {void*} p 对象指针
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t feature_info_list_init_features(feature_info_list_t* list, void* p);
+
+/**
+ * @method feature_info_list_deinit_features
+ * 反初始化对象的特征
+ * > 需要增加特征的类内部使用，外部不应该调用此接口。
+ * @param {feature_info_list_t*} list 特征信息列表对象
+ * @param {void*} p 对象指针
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t feature_info_list_deinit_features(feature_info_list_t* list, void* p);
+
+#define FEATURE_INFO_LIST_ITEM_GET_FEATURE(item, p) ((uint8_t*)(p) + (item)->offset)
+
+END_C_DECLS
+
+#endif /*TK_FEATURE_INFO_LIST_H*/
