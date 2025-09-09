e5d2f6465798417f4963fee29ab2f90f36f70f5e
lixianjing <xianjimli@hotmail.com>
2025-09-10 07:14:57 +0800

    improve locale info xml

--- src/base/locale_info_xml.c
+++ src/base/locale_info_xml.c
@@ -49,6 +49,8 @@ static int32_t locale_info_xml_get_node_size(locale_info_xml_t* xml_info, conf_n
 static conf_node_t* locale_info_xml_get_node_by_text(locale_info_t* locale_info, const char* text) {
   value_t v;
   char path[MAX_PATH + 1] = {0};
+  const char* name = NULL;
+  conf_node_t* name_node = NULL;
   locale_info_xml_t* xml_info = (locale_info_xml_t*)locale_info;
   int32_t size = locale_info_xml_get_node_size(xml_info, NULL);
 
@@ -56,12 +58,12 @@ static conf_node_t* locale_info_xml_get_node_by_text(locale_info_t* locale_info,
     memset(path, 0, sizeof(path));
     tk_snprintf(path, sizeof(path), "root.[%d].name", i);
 
-    conf_node_t* name_node = conf_doc_find_node(xml_info->doc, NULL, path, FALSE);
-    if (conf_node_get_value(name_node, &v) != RET_OK) {
+    name_node = conf_doc_find_node(xml_info->doc, NULL, path, FALSE);
+    if (name_node == NULL || conf_node_get_value(name_node, &v) != RET_OK) {
       continue;
     }
-    const char* name = value_str(&v);
 
+    name = value_str(&v);
     if (tk_str_eq(name, text)) {
       return name_node->parent;
     }
@@ -74,18 +76,21 @@ static conf_node_t* locale_info_xml_get_node_by_language(locale_info_t* locale_i
                                                          conf_node_t* node, const char* language) {
   value_t v;
   char path[MAX_PATH + 1] = {0};
+  const char* name = NULL;
+  conf_node_t* name_node = NULL;
   locale_info_xml_t* xml_info = (locale_info_xml_t*)locale_info;
   int32_t size = locale_info_xml_get_node_size(xml_info, node);
 
   for (int32_t i = 0; i < size; i++) {
     memset(path, 0, sizeof(path));
     tk_snprintf(path, sizeof(path), "[%d].name", i);
-    conf_node_t* name_node = conf_doc_find_node(xml_info->doc, node, path, FALSE);
-    if (conf_node_get_value(name_node, &v) != RET_OK) {
+
+    name_node = conf_doc_find_node(xml_info->doc, node, path, FALSE);
+    if (name_node == NULL || conf_node_get_value(name_node, &v) != RET_OK) {
       continue;
     }
-    const char* name = value_str(&v);
 
+    name = value_str(&v);
     if (tk_str_eq(name, language)) {
       return name_node->parent;
     }
@@ -140,6 +145,7 @@ locale_info_t* locale_info_xml_create(const char* language, const char* country)
   xml_info->text_value = value_create();
 
   info = (locale_info_t*)xml_info;
+  info->assets_manager = NULL;
   locale_info_set_fallback_tr2(info, locale_info_xml_get_tr, info);
 
   return info;
@@ -161,63 +167,107 @@ static str_t* get_strings_content(const char* path, str_t* result) {
   return result;
 }
 
-static ret_t locale_info_xml_reload(locale_info_xml_t* xml_info) {
-  if (xml_info == NULL) {
-    return RET_BAD_PARAMS;
-  }
+static ret_t locale_info_xml_reload(locale_info_t* locale_info, const char* url) {
+  locale_info_xml_t* xml_info = (locale_info_xml_t*)locale_info;
+
+  assert(xml_info->strings_url != NULL);
 
   if (xml_info->doc != NULL) {
     conf_doc_destroy(xml_info->doc);
+    xml_info->doc = NULL;
   }
 
-  str_t* content = str_create(0);
-  content = get_strings_content(xml_info->strings_url->str, content);
+  str_set(xml_info->strings_url, url);
 
-  xml_info->doc = conf_doc_load_xml(content->str);
-  str_destroy(content);
+  if (TK_STR_IS_NOT_EMPTY(url)) {
+    str_t* content = str_create(0);
+    content = get_strings_content(url, content);
+    xml_info->doc = conf_doc_load_xml(content->str);
+    str_destroy(content);
+  }
 
   return RET_OK;
 }
 
-ret_t locale_info_xml_set_url(locale_info_t* locale_info, const char* strings_url) {
+ret_t locale_info_xml_set_url(locale_info_t* locale_info, const char* url) {
+  return_value_if_fail(locale_info != NULL, RET_BAD_PARAMS);
+
+  return locale_info_xml_reload(locale_info, url);
+}
+
+static ret_t locale_info_xml_on_asset_manager_clear_cache(void* ctx, event_t* e) {
+  locale_info_t* locale_info = (locale_info_t*)ctx;
   locale_info_xml_t* xml_info = (locale_info_xml_t*)locale_info;
-  if (xml_info == NULL) {
-    return RET_BAD_PARAMS;
-  }
+  assets_event_t* evt = (assets_event_t*)e;
 
-  str_set(xml_info->strings_url, strings_url);
-  locale_info_xml_reload(xml_info);
+  if (evt->type == ASSET_TYPE_STRINGS) {
+    return locale_info_xml_reload(locale_info, xml_info->strings_url->str);
+  }
 
   return RET_OK;
 }
 
-ret_t locale_info_xml_set_assets_manager(locale_info_t* locale_info,
-                                         assets_manager_t* assets_manager) {
-  ret_t ret = locale_info_set_assets_manager(locale_info, assets_manager);
-  if (ret != RET_OK) {
-    return ret;
+ret_t locale_info_xml_set_assets_manager(locale_info_t* locale_info, assets_manager_t* am) {
+  const char* res_root = am != NULL ? assets_manager_get_res_root(am) : NULL;
+  const char* theme = am != NULL ? assets_manager_get_theme_name(am) : NULL;
+  return_value_if_fail(locale_info != NULL, RET_BAD_PARAMS);
+
+  if (locale_info->assets_manager != NULL) {
+    emitter_off_by_ctx(EMITTER(locale_info->assets_manager), locale_info);
   }
 
-  const char* res_root = assets_manager_get_res_root(assets_manager);
-  const char* theme = assets_manager_get_theme_name(assets_manager);
+  return_value_if_fail(locale_info_set_assets_manager(locale_info, am) == RET_OK, RET_FAIL);
 
-  if (res_root != NULL && theme != NULL) {
+  if (TK_STR_IS_EMPTY(theme)) {
+    theme = "default";
+  }
+
+  if (TK_STR_IS_NOT_EMPTY(res_root)) {
     char path[MAX_PATH + 1] = {0};
     tk_snprintf(path, sizeof(path), "%s/%s/strings/strings.xml", res_root, theme);
-    locale_info_xml_set_url(locale_info, path);
+    locale_info_xml_reload(locale_info, path);
+  } else {
+    locale_info_xml_reload(locale_info, NULL);
   }
 
-  return ret;
+  if (am != NULL) {
+    emitter_on(EMITTER(am), EVT_ASSET_MANAGER_CLEAR_CACHE,
+               locale_info_xml_on_asset_manager_clear_cache, locale_info);
+  }
+
+  return RET_OK;
 }
 
 ret_t locale_info_xml_destroy(locale_info_t* locale_info) {
   locale_info_xml_t* xml_info = (locale_info_xml_t*)locale_info;
 
-  conf_doc_destroy(xml_info->doc);
-  str_destroy(xml_info->strings_url);
-  str_destroy(xml_info->language);
-  value_destroy(xml_info->text_value);
-  locale_info_destroy(locale_info);
+  if (xml_info != NULL) {
+    if (locale_info->assets_manager != NULL) {
+      emitter_off_by_ctx(EMITTER(locale_info->assets_manager), locale_info);
+    }
+
+    if (xml_info->doc != NULL) {
+      conf_doc_destroy(xml_info->doc);
+      xml_info->doc = NULL;
+    }
+
+    if (xml_info->strings_url != NULL) {
+      str_destroy(xml_info->strings_url);
+      xml_info->strings_url = NULL;
+    }
+
+    if (xml_info->language != NULL) {
+      str_destroy(xml_info->language);
+      xml_info->language = NULL;
+    }
+
+    if (xml_info->text_value != NULL) {
+      value_destroy(xml_info->text_value);
+      xml_info->text_value = NULL;
+    }
+
+    locale_info_destroy(locale_info);
+  }
 
   return RET_OK;
 }
