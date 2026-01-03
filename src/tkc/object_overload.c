97240aae5064947a9075ac97a9a63cbde134aefd
lixianjing <xianjimli@hotmail.com>
2026-01-03 17:01:15 +0800

    add object_overload

---
+++ src/tkc/object_overload.c
@@ -0,0 +1,230 @@
+﻿/**
+ * File:   object_overload.c
+ * Author: AWTK Develop Team
+ * Brief:  对象方法重载器。
+ *
+ * Copyright (c) 2025 - 2025 Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2024-12-31 Shen ZhaoKun <shenzhaokun@zlg.cn> created
+ *
+ */
+
+#include "object_overload.h"
+
+#include "tkc/mem.h"
+#include "tkc/utils.h"
+
+static ret_t object_overload_on_destroy(tk_object_t* obj) {
+  ret_t ret = RET_OK;
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->on_destroy != NULL) {
+    ret = overload->overload_vt->on_destroy(overload->base_obj);
+  }
+
+  TK_OBJECT_UNREF(overload->base_obj);
+
+  return ret;
+}
+
+static int32_t object_overload_compare(tk_object_t* obj, tk_object_t* other) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, -1);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->compare != NULL) {
+    return overload->overload_vt->compare(overload->base_obj, other);
+  }
+
+  return tk_object_compare(overload->base_obj, other);
+}
+
+static ret_t object_overload_get_prop(tk_object_t* obj, const char* name, value_t* v) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->get_prop != NULL) {
+    return overload->overload_vt->get_prop(overload->base_obj, name, v);
+  }
+
+  return tk_object_get_prop(overload->base_obj, name, v);
+}
+
+static ret_t object_overload_set_prop(tk_object_t* obj, const char* name, const value_t* v) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->set_prop != NULL) {
+    return overload->overload_vt->set_prop(overload->base_obj, name, v);
+  }
+
+  return tk_object_set_prop(overload->base_obj, name, v);
+}
+
+static ret_t object_overload_copy_props(tk_object_t* obj, tk_object_t* src, bool_t overwrite) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->copy_props != NULL) {
+    return overload->overload_vt->copy_props(overload->base_obj, src, overwrite);
+  }
+
+  return tk_object_copy_props(overload->base_obj, src, overwrite);
+}
+
+static ret_t object_overload_remove_prop(tk_object_t* obj, const char* name) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->remove_prop != NULL) {
+    return overload->overload_vt->remove_prop(overload->base_obj, name);
+  }
+
+  return tk_object_remove_prop(overload->base_obj, name);
+}
+
+static ret_t object_overload_foreach_prop(tk_object_t* obj, tk_visit_t on_prop, void* ctx) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->foreach_prop != NULL) {
+    return overload->overload_vt->foreach_prop(overload->base_obj, on_prop, ctx);
+  }
+
+  return tk_object_foreach_prop(overload->base_obj, on_prop, ctx);
+}
+
+static ret_t object_overload_clear_props(tk_object_t* obj) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->clear_props != NULL) {
+    return overload->overload_vt->clear_props(overload->base_obj);
+  }
+
+  return tk_object_clear_props(overload->base_obj);
+}
+
+static value_t* object_overload_find_prop(tk_object_t* obj, tk_compare_t cmp, const void* ctx) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, NULL);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->find_prop != NULL) {
+    return overload->overload_vt->find_prop(overload->base_obj, cmp, ctx);
+  }
+
+  return tk_object_find_prop(overload->base_obj, cmp, ctx);
+}
+
+static ret_t object_overload_find_props(tk_object_t* obj, tk_compare_t cmp, const void* ctx,
+                                        darray_t* matched) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->find_props != NULL) {
+    return overload->overload_vt->find_props(overload->base_obj, cmp, ctx, matched);
+  }
+
+  return tk_object_find_props(overload->base_obj, cmp, ctx, matched);
+}
+
+static bool_t object_overload_can_exec(tk_object_t* obj, const char* name, const char* args) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, FALSE);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->can_exec != NULL) {
+    return overload->overload_vt->can_exec(overload->base_obj, name, args);
+  }
+
+  return tk_object_can_exec(overload->base_obj, name, args);
+}
+
+static ret_t object_overload_exec(tk_object_t* obj, const char* name, const char* args) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->exec != NULL) {
+    return overload->overload_vt->exec(overload->base_obj, name, args);
+  }
+
+  return tk_object_exec(overload->base_obj, name, args);
+}
+
+static ret_t object_overload_exec_ex(tk_object_t* obj, const char* name, const char* args,
+                                     value_t* result) {
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, RET_BAD_PARAMS);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->exec_ex != NULL) {
+    return overload->overload_vt->exec_ex(overload->base_obj, name, args, result);
+  }
+
+  return tk_object_exec_ex(overload->base_obj, name, args, result);
+}
+
+static tk_object_t* object_overload_clone(tk_object_t* obj) {
+  tk_object_t* ret = NULL;
+  tk_object_t* clone_base_obj = NULL;
+  object_overload_t* overload = OBJECT_OVERLOAD(obj);
+  return_value_if_fail(overload != NULL, NULL);
+
+  if (overload->overload_vt != NULL && overload->overload_vt->clone != NULL) {
+    clone_base_obj = overload->overload_vt->clone(overload->base_obj);
+  } else {
+    clone_base_obj = tk_object_clone(overload->base_obj);
+  }
+  return_value_if_fail(clone_base_obj != NULL, NULL);
+
+  ret = object_overload_create(clone_base_obj, overload->overload_vt);
+
+  tk_object_unref(clone_base_obj);
+  return ret;
+}
+
+static const object_vtable_t s_object_overload_vtable = {
+    .type = OBJECT_OVERLOAD_TYPE,
+    .desc = OBJECT_OVERLOAD_TYPE,
+    .size = sizeof(object_overload_t),
+    .on_destroy = object_overload_on_destroy,
+    .compare = object_overload_compare,
+    .get_prop = object_overload_get_prop,
+    .set_prop = object_overload_set_prop,
+    .copy_props = object_overload_copy_props,
+    .remove_prop = object_overload_remove_prop,
+    .foreach_prop = object_overload_foreach_prop,
+    .clear_props = object_overload_clear_props,
+    .find_prop = object_overload_find_prop,
+    .find_props = object_overload_find_props,
+    .can_exec = object_overload_can_exec,
+    .exec = object_overload_exec,
+    .exec_ex = object_overload_exec_ex,
+    .clone = object_overload_clone,
+};
+
+tk_object_t* object_overload_create(tk_object_t* base_obj, const object_vtable_t* overload_vt) {
+  object_overload_t* ret = NULL;
+  return_value_if_fail(base_obj != NULL, NULL);
+
+  ret = (object_overload_t*)tk_object_create(&s_object_overload_vtable);
+  return_value_if_fail(ret != NULL, NULL);
+
+  ret->base_obj = TK_OBJECT_REF(base_obj);
+  ret->overload_vt = overload_vt;
+
+  return TK_OBJECT(ret);
+}
+
+object_overload_t* object_overload_cast(tk_object_t* obj) {
+  return_value_if_fail(obj != NULL && obj->vt == &s_object_overload_vtable, NULL);
+  return (object_overload_t*)obj;
+}
---
+++ tests/object_overload_test.cc
@@ -0,0 +1,143 @@
+﻿#include "gtest/gtest.h"
+#include "tkc/object_overload.h"
+#include "tkc/object_default.h"
+
+static ret_t s_overload_get_prop(tk_object_t* obj, const char* name, value_t* v) {
+  if (tk_str_eq(name, "overloaded_prop")) {
+    value_set_str(v, "overloaded_value");
+    return RET_OK;
+  }
+  return RET_NOT_FOUND;
+}
+
+static ret_t s_overload_set_prop(tk_object_t* obj, const char* name, const value_t* v) {
+  if (tk_str_eq(name, "overloaded_prop")) {
+    return RET_OK;
+  }
+  return RET_NOT_FOUND;
+}
+
+static bool_t s_overload_can_exec(tk_object_t* obj, const char* name, const char* args) {
+  if (tk_str_eq(name, "overloaded_cmd")) {
+    return TRUE;
+  } else if (tk_str_eq(name, "overloaded_cmd_ex")) {
+    return TRUE;
+  }
+  return FALSE;
+}
+
+static ret_t s_overload_exec(tk_object_t* obj, const char* name, const char* args) {
+  if (tk_str_eq(name, "overloaded_cmd")) {
+    return RET_OK;
+  }
+  return RET_NOT_FOUND;
+}
+
+static ret_t s_overload_exec_ex(tk_object_t* obj, const char* name, const char* args,
+                                value_t* result) {
+  if (tk_str_eq(name, "overloaded_cmd_ex")) {
+    value_set_str(result, "overloaded_result");
+    return RET_OK;
+  }
+  return RET_NOT_FOUND;
+}
+
+static const struct test_overload_vtable_t : object_vtable_t {
+  test_overload_vtable_t() {
+    type = "test_overload";
+    desc = "test_overload";
+    size = sizeof(tk_object_t);
+    is_collection = FALSE;
+    get_prop = s_overload_get_prop;
+    set_prop = s_overload_set_prop;
+    can_exec = s_overload_can_exec;
+    exec = s_overload_exec;
+    exec_ex = s_overload_exec_ex;
+  }
+} s_test_overload_vt;
+
+TEST(ObjectOverload, basic) {
+  tk_object_t* base_obj = object_default_create();
+  tk_object_t* overload_obj = object_overload_create(base_obj, &s_test_overload_vt);
+  ASSERT_TRUE(overload_obj != NULL);
+
+  object_overload_t* overload = OBJECT_OVERLOAD(overload_obj);
+  ASSERT_TRUE(overload != NULL);
+
+  ASSERT_EQ(overload->base_obj, base_obj);
+  ASSERT_EQ(overload->overload_vt, &s_test_overload_vt);
+
+  TK_OBJECT_UNREF(overload_obj);
+  TK_OBJECT_UNREF(base_obj);
+}
+
+TEST(ObjectOverload, prop) {
+  tk_object_t* base_obj = object_default_create();
+  tk_object_t* overload_obj = object_overload_create(base_obj, &s_test_overload_vt);
+  value_t v1 = {};
+
+  ASSERT_EQ(tk_object_get_prop(overload_obj, "overloaded_prop", &v1), RET_OK);
+  ASSERT_STREQ(value_str(&v1), "overloaded_value");
+
+  ASSERT_EQ(tk_object_get_prop(overload_obj, "non_exist_prop", &v1), RET_NOT_FOUND);
+
+  TK_OBJECT_UNREF(overload_obj);
+  TK_OBJECT_UNREF(base_obj);
+}
+
+TEST(ObjectOverload, exec) {
+  tk_object_t* base_obj = object_default_create();
+  tk_object_t* overload_obj = object_overload_create(base_obj, &s_test_overload_vt);
+  value_t result = {};
+
+  ASSERT_TRUE(tk_object_can_exec(overload_obj, "overloaded_cmd", NULL));
+  ASSERT_EQ(tk_object_exec(overload_obj, "overloaded_cmd", NULL), RET_OK);
+
+  ASSERT_TRUE(tk_object_can_exec(overload_obj, "overloaded_cmd_ex", NULL));
+  ASSERT_EQ(tk_object_exec_ex(overload_obj, "overloaded_cmd_ex", NULL, &result), RET_OK);
+  ASSERT_STREQ(value_str(&result), "overloaded_result");
+  value_reset(&result);
+
+  ASSERT_FALSE(tk_object_can_exec(overload_obj, "non_exist_cmd", NULL));
+  ASSERT_EQ(tk_object_exec(overload_obj, "non_exist_cmd", NULL), RET_NOT_FOUND);
+
+  TK_OBJECT_UNREF(overload_obj);
+  TK_OBJECT_UNREF(base_obj);
+}
+
+TEST(ObjectOverload, clone) {
+  tk_object_t* base_obj = object_default_create();
+  tk_object_t* overload_obj = object_overload_create(base_obj, &s_test_overload_vt);
+  tk_object_t* clone_obj = NULL;
+  value_t v = {};
+
+  value_set_str(&v, "base_value");
+  ASSERT_EQ(tk_object_set_prop(base_obj, "base_prop", &v), RET_OK);
+
+  clone_obj = tk_object_clone(overload_obj);
+  ASSERT_TRUE(clone_obj != NULL);
+  object_overload_t* clone = OBJECT_OVERLOAD(clone_obj);
+  ASSERT_NE(base_obj, clone->base_obj);
+
+  ASSERT_EQ(tk_object_get_prop(clone_obj, "overloaded_prop", &v), RET_OK);
+  ASSERT_STREQ(value_str(&v), "overloaded_value");
+
+  TK_OBJECT_UNREF(clone_obj);
+  TK_OBJECT_UNREF(overload_obj);
+  TK_OBJECT_UNREF(base_obj);
+}
+
+TEST(ObjectOverload, null_overload_vt) {
+  tk_object_t* base_obj = object_default_create();
+  tk_object_t* overload_obj = object_overload_create(base_obj, NULL);
+  value_t v1 = {};
+  value_t v2 = {};
+
+  value_set_str(&v1, "base_value");
+  ASSERT_EQ(tk_object_set_prop(base_obj, "base_prop", &v1), RET_OK);
+  ASSERT_EQ(tk_object_get_prop(overload_obj, "base_prop", &v2), RET_OK);
+  ASSERT_STREQ(value_str(&v1), value_str(&v2));
+
+  TK_OBJECT_UNREF(overload_obj);
+  TK_OBJECT_UNREF(base_obj);
+}
