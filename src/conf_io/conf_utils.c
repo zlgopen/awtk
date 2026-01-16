79cdd688afe4c7d842d930b3ecabf4d7a430927a
lixianjing <xianjimli@hotmail.com>
2026-01-16 10:57:24 +0800

    add object_load_conf

--- src/conf_io/conf_utils.c
+++ src/conf_io/conf_utils.c
@@ -27,14 +27,11 @@
 #include "tkc/data_reader_mem.h"
 
 #include "conf_io/conf_json.h"
+#include "conf_io/conf_yaml.h"
 
-ret_t object_to_json(tk_object_t* obj, str_t* str) {
-  return tk_object_to_json(obj, str, 2, 0, FALSE);
-}
+static ret_t object_load_conf_copy_props(tk_object_t* obj, tk_object_t* src);
 
-static ret_t object_from_json_copy_props(tk_object_t* obj, tk_object_t* src);
-
-static ret_t object_from_json_set_prop(tk_object_t* obj, const char* name, const value_t* v) {
+static ret_t object_load_conf_set_prop(tk_object_t* obj, const char* name, const value_t* v) {
   ret_t ret = RET_OK;
   value_t value;
 
@@ -42,12 +39,12 @@ static ret_t object_from_json_set_prop(tk_object_t* obj, const char* name, const
     value_t tmp;
     tk_object_t* o = NULL;
     tk_object_t* v_obj = value_object(v);
-    if (v_obj->vt->is_collection) {
+    if (tk_object_is_collection(v_obj)) {
       o = object_array_create();
     } else {
       o = object_hash_create();
     }
-    object_from_json_copy_props(o, v_obj);
+    object_load_conf_copy_props(o, v_obj);
     value_set_object(&tmp, o);
     value_deep_copy(&value, &tmp);
     TK_OBJECT_UNREF(o);
@@ -66,33 +63,57 @@ static ret_t object_from_json_set_prop(tk_object_t* obj, const char* name, const
   return ret;
 }
 
-static ret_t object_from_json_copy_props_on_visit(void* ctx, const void* data) {
+static ret_t object_load_conf_copy_props_on_visit(void* ctx, const void* data) {
   named_value_t* nv = (named_value_t*)data;
   tk_object_t* obj = TK_OBJECT(ctx);
-  object_from_json_set_prop(obj, nv->name, &(nv->value));
+  object_load_conf_set_prop(obj, nv->name, &(nv->value));
   return RET_OK;
 }
 
-inline static ret_t object_from_json_copy_props(tk_object_t* obj, tk_object_t* src) {
+inline static ret_t object_load_conf_copy_props(tk_object_t* obj, tk_object_t* src) {
   return_value_if_fail(obj != NULL && src != NULL, RET_BAD_PARAMS);
 
-  return tk_object_foreach_prop(src, object_from_json_copy_props_on_visit, obj);
+  return tk_object_foreach_prop(src, object_load_conf_copy_props_on_visit, obj);
+}
+
+static tk_object_t* conf_obj_load_ex(const char* url, bool_t create_if_not_exist,
+                                     bool_t use_extend_type, const char* type) {
+  if (TK_STR_IS_EMPTY(type)) {
+    type = strrchr(url, '.');
+    if (type != NULL) {
+      type++;
+    }
+  }
+  if (tk_str_ieq("json", type)) {
+    return conf_json_load_ex(url, create_if_not_exist, use_extend_type);
+  } else if (tk_str_ieq("yaml", type)) {
+    return conf_yaml_load_ex(url, create_if_not_exist, use_extend_type);
+  }
+  return NULL;
+}
+
+ret_t object_load_conf(tk_object_t* obj, const char* url, const char* type) {
+  ret_t ret = RET_OK;
+  tk_object_t* obj_conf = NULL;
+  return_value_if_fail(obj != NULL && TK_STR_IS_NOT_EMPTY(url), RET_BAD_PARAMS);
+
+  obj_conf = conf_obj_load_ex(url, FALSE, TRUE, type);
+  return_value_if_fail(obj_conf != NULL, RET_FAIL);
+
+  ret = object_load_conf_copy_props(obj, obj_conf);
+
+  TK_OBJECT_UNREF(obj_conf);
+
+  return ret;
+}
+
+ret_t object_to_json(tk_object_t* obj, str_t* str) {
+  return tk_object_to_json(obj, str, 2, 0, FALSE);
 }
 
 ret_t object_from_json(tk_object_t* obj, const char* str) {
-  ret_t ret = RET_OK;
   char url[MAX_PATH + 1];
-  tk_object_t* obj_json = NULL;
   return_value_if_fail(obj != NULL && str != NULL, RET_BAD_PARAMS);
 
-  data_reader_mem_build_url(str, tk_strlen(str), url);
-  obj_json = conf_json_load_ex(url, FALSE, TRUE);
-  return_value_if_fail(obj_json != NULL, RET_OOM);
-
-  tk_object_clear_props(obj);
-  ret = object_from_json_copy_props(obj, obj_json);
-
-  TK_OBJECT_UNREF(obj_json);
-
-  return ret;
+  return object_load_conf(obj, data_reader_mem_build_url(str, tk_strlen(str), url), "json");
 }
--- src/conf_io/conf_utils.h
+++ src/conf_io/conf_utils.h
@@ -28,14 +28,28 @@ BEGIN_C_DECLS
 
 /**
  * @class conf_utils_t
- * @annotation ["fake"]
+ * @annotation ["fake", "scriptable"]
  * 工具类。
  *
  */
 
 /**
+ * @method object_load_conf
+ * 加载配置文件到对象中。
+ * @annotation ["static", "scriptable"]
+ *
+ * @param {tk_object_t*} obj object对象。
+ * @param {const char*} url 配置文件路径。
+ * @param {const char*} type 配置文件类型, 如果为NULL，则自动检测。
+ *
+ * @return {ret_t} 返回RET_OK表示成功，否则表示失败。
+ */
+ret_t object_load_conf(tk_object_t* obj, const char* url, const char* type);
+
+/**
  * @method object_to_json
  * 将对象转换成json字符串。
+ * @annotation ["static"]
  *
  * @param {tk_object_t*} obj object对象。
  * @param {str_t*} str 用于返回结果。
@@ -47,6 +61,7 @@ ret_t object_to_json(tk_object_t* obj, str_t* str);
 /**
  * @method object_from_json
  * 将json字符串转换成对象。
+ * @annotation ["static"]
  *
  * @param {tk_object_t*} obj object对象, 用于返回结果。
  * @param {const char*} str json字符串。
