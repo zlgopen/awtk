6928d506b8e73d86b5ade8aadff8059f3b4bdd51
lixianjing <xianjimli@hotmail.com>
2026-04-01 17:15:22 +0800

    improve font_gen

--- tools/font_gen/expand_text.c
+++ tools/font_gen/expand_text.c
@@ -3,15 +3,24 @@
 #include "tkc/platform.h"
 #include "font_gen/preprocess_text.inc"
 
-int main(int argc, char* argv[]) {
+int wmain(int argc, wchar_t* argv[]) {
   if (argc != 3) {
     printf("Usage:%s input_file output_file\n", argv[0]);
   } else {
     uint32_t size = 0;
-    const char* input_file = argv[1];
-    const char* output_file = argv[2];
-    void* data = file_read(input_file, &size);
+    str_t str_input_file = {0};
+    str_t str_output_file = {0};
+    const char* input_file = NULL;
+    const char* output_file = NULL;
 
+    str_init(&str_input_file, 0);
+    str_init(&str_output_file, 0);
+    str_from_wstr(&str_input_file, argv[1]);
+    str_from_wstr(&str_output_file, argv[2]);
+    input_file = str_input_file.str;
+    output_file = str_output_file.str;
+
+    void* data = file_read(input_file, &size);
     if (data == NULL) {
       printf("file %s not found\n", input_file);
     } else {
@@ -28,3 +37,5 @@ int main(int argc, char* argv[]) {
   }
   return 0;
 }
+
+#include "common/main.inc"
